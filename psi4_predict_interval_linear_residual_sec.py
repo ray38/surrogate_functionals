aaf18b9477d992f8e433caa68d5163bac1d5ab9f
Ray Lei <xlei38@gatech.edu>
2017-10-26 14:41:03 -0400

    added prediction

--- psi4_predict_interval_linear_residual_sec.py
+++ psi4_predict_interval_linear_residual_sec.py
@@ -40,9 +40,10 @@ def load_data_each_block(molecule,functional,i,j,k, target, gamma, num_desc_deri
     data_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
     print data_filename
     data =  h5py.File(data_filename,'r')
-    r_list = [0.2,0.3,0.4,0.5,0.6,1.0]
+    r_list = [0.04,0.06,0.08,0.1,0.2,0.3,0.4,0.5]
     
     result_list = []
+    y = []
   
     
     if target == 'Vxc':
@@ -52,14 +53,15 @@ def load_data_each_block(molecule,functional,i,j,k, target, gamma, num_desc_deri
     if target == 'tau':
         target_set_name = 'tau'
     
-    y = np.asarray(data[target_set_name])
-    temp_data = np.asarray([])
+#    y = np.asarray(data[target_set_name])
+#    temp_data = np.asarray([])
+    temp_data = np.asarray(data[target_set_name])
     
     
     if target_transform == 'real':
-        result_list.append(temp_data.flatten().tolist())
+        y.append(temp_data.flatten().tolist())
     elif target_transform == 'log':
-        result_list.append(np.log10(np.multiply(-1., temp_data.flatten())).tolist())
+        y.append(np.log10(np.multiply(-1., temp_data.flatten())).tolist())
 
     
     temp_data = np.asarray(data['rho'])
@@ -106,9 +108,10 @@ def load_data_each_block(molecule,functional,i,j,k, target, gamma, num_desc_deri
     
     
     result = zip(*result_list)
+    y = zip(*y)
 
 
-    return result, y
+    return np.asarray(result), np.asarray(y)
 
 def predict_each_block(model_dict, X, y):
     temp_X_li = []
@@ -116,6 +119,9 @@ def predict_each_block(model_dict, X, y):
     for i in range(len(model_dict)):
         temp_X_li.append([])
         temp_y_li.append([])
+    print X[0]
+    print y[0]
+#    print model_dict[key]['X'][:,0]
     X_temp = X.tolist()
     y_temp = y.reshape(len(y)).tolist()
     
@@ -131,10 +137,10 @@ def predict_each_block(model_dict, X, y):
             temp_count +=1
     temp_count = 0
 
-#    for key in model_dict:
-#        model_dict[key]['X'] = np.asarray(temp_X_li[temp_count])
-#        model_dict[key]['y'] = np.asarray(temp_y_li[temp_count])
-#        temp_count += 1
+    for key in model_dict:
+        model_dict[key]['X'] = np.asarray(temp_X_li[temp_count])
+        model_dict[key]['y'] = np.asarray(temp_y_li[temp_count])
+        temp_count += 1
     
     
     original_y = np.asarray([])
@@ -143,6 +149,7 @@ def predict_each_block(model_dict, X, y):
     
     for key in model_dict:
         original_y = np.append(original_y, model_dict[key]['y'])
+#        print model_dict[key]['X']
         temp_dens = model_dict[key]['X'][:,0]
         temp = model_dict[key]['li_model'].predict(temp_dens.reshape(len(temp_dens),1)) + model_dict[key]['model'].predict(model_dict[key]['X'])
 #        predict_y = np.multiply(-1.,(np.power(10.,temp)))
@@ -166,11 +173,12 @@ def predict_each_block(model_dict, X, y):
 def process_one_molecule(molecule, model_dict, functional,h,N,log_filename, target,gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens):
     cwd = os.getcwd()
     dir_name = "{}_{}_{}_{}_{}".format(molecule,functional,str(L).replace('.','-'),str(h).replace('.','-'),N)
+    print cwd
     print dir_name
     
-    if os.path.isdir(dir_name) == False:
-        print '\n****Error: Cant find the directory! ****\n'
-        raise NotImplementedError
+#    if os.path.isdir(cwd + '/'+dir_name) == False:
+#        print '\n****Error: Cant find the directory! ****\n'
+#        raise NotImplementedError
     
     os.chdir(cwd + '/' + dir_name)
     Nx = Ny = Nz = N
@@ -184,6 +192,7 @@ def process_one_molecule(molecule, model_dict, functional,h,N,log_filename, targ
     
     paramlist = list(itertools.product(i_li,j_li,k_li))
     for i,j,k in paramlist:
+        
         sum_error, y_predict_sum, y_sum = process_each_block(molecule, model_dict, functional,i,j,k,h,N,log_filename, target,gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens)
         system_sum_error += sum_error
         system_y_predict_sum += y_predict_sum
@@ -206,7 +215,8 @@ def process_one_molecule(molecule, model_dict, functional,h,N,log_filename, targ
     return system_sum_error, system_y_predict_sum,system_y_sum
     
 def process_each_block(molecule, model_dict, functional,i,j,k,h,N,log_filename, target,gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens):
-    log(log_filename,"\n\nstart loading: " + molecule + "\t" + i + "\t" + j + "\t" + k)
+    print 'started'    
+    log(log_filename,"\n\nstart loading: " + molecule + "\t" + str(i) + "\t" + str(j) + "\t" + str(k))
     start = time.time()
     X,y = load_data_each_block(molecule,functional,i,j,k, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens)
     original_y, predict_y = predict_each_block(model_dict, X, y)
@@ -221,13 +231,13 @@ def process_each_block(molecule, model_dict, functional,i,j,k,h,N,log_filename, 
     sum_error = y_sum - y_predict_sum
 
     
-    log(log_filename,"\ndone predicting: " + molecule + "\t took time: " + str(time.time()-start)+ "\t" + i + "\t" + j + "\t" + k)
+    log(log_filename,"\ndone predicting: " + molecule + "\t took time: " + str(time.time()-start)+ "\t" + str(i) + "\t" + str(j) + "\t" + str(k))
     log(log_filename,"\nenergy: " + str(y_sum) + "\tpredicted energy: " + str(y_predict_sum) + "\tpredicted error: " + str(sum_error))    
 
     return sum_error, y_predict_sum, y_sum    
 
 
-def read_model_inputfile(input_filename,h,L,N):
+def read_model_inputfile(input_filename,h,L,N,target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens):
     model_dict = {}
     interval_divide = []
     with open(input_filename,'r') as f:
@@ -238,7 +248,7 @@ def read_model_inputfile(input_filename,h,L,N):
             temp_info = model_name.replace('.','_').split('_')
             
             cwd = os.getcwd()
-            result_dir = "{}_{}_{}_{}_{}_gamma{}_dev{}_devsq{}_inte{}_{}_{}_models".format(functional,str(L).replace('.','-'),str(h).replace('.','-'),N,target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,temp_info[10],temp_info[11])
+            result_dir = "{}_{}_{}_{}_{}_gamma{}_dev{}_devsq{}_inte{}_log_log_models".format(functional,str(L).replace('.','-'),str(h).replace('.','-'),N,target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,temp_info[10],temp_info[11])
             os.chdir(cwd + '/' + result_dir)            
             temp_model = load_model(model_name)
             li_model = pickle.load(open(li_model_name, 'rb'))
@@ -253,12 +263,12 @@ def read_model_inputfile(input_filename,h,L,N):
             model_dict[temp[0]]['li_model'] = li_model
             model_dict[temp[0]]['desc_transform'] = temp_info[10]
             model_dict[temp[0]]['ener_transform'] = temp_info[11]
-            model_dict[temp[0]]['num_desc'] = int(temp_info[7][3:])
+#            model_dict[temp[0]]['num_desc'] = int(temp_info[7][3:])
             model_dict[temp[0]]['activation'] = temp_info[6]
             model_dict[temp[0]]['X'] = np.asarray([])
             model_dict[temp[0]]['y'] = np.asarray([])
-            model_dict[temp[0]]['model_start'] = float(temp_info[8].replace('n','-'))
-            model_dict[temp[0]]['model_end']   = float(temp_info[9].replace('n','-'))
+            model_dict[temp[0]]['model_start'] = float(temp[8])
+            model_dict[temp[0]]['model_end']   = float(temp[9])
             #            model_dict[temp[0]]['predicted_y'] = []
             interval_divide.append([float(temp[1]), float(temp[2])])
 
@@ -284,7 +294,7 @@ num_desc_deri_squa = int(sys.argv[11])
 num_desc_ave_dens = int(sys.argv[12])
 
 
-model_dict, interval_divide = read_model_inputfile(model_setup_filename,h,L,N)
+model_dict, interval_divide = read_model_inputfile(model_setup_filename,h,L,N,target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens)
 
 
 log(log_filename,'\n\n***************\n' + pprint.pformat(model_dict, indent=4) + '\n---')
@@ -297,11 +307,11 @@ molecule_names = [x.strip() for x in molecule_names]
 error_list = []
 
 for molecule in molecule_names:
-    try:
-        temp_error,temp_y_predict,temp_y = process_one_molecule(molecule, model_dict, functional,h,N,log_filename, target,gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens)
-        error_list.append(temp_error)
-    except:
-        log(log_filename,"\n\n Failed") 
+#    try:
+    temp_error,temp_y_predict,temp_y = process_one_molecule(molecule, model_dict, functional,h,N,log_filename, target,gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens)
+    error_list.append(temp_error)
+#    except:
+#        log(log_filename,"\n\n Failed") 
         
 log(log_filename,"\n\naverage error: " + str(np.mean(error_list)) + "\tstddev error: " + str(np.std(error_list))) 
 log(log_filename,"\n\naverage abs error: " + str(np.mean(np.abs(error_list))) + "\tstddev abs error: " + str(np.std(np.abs(error_list)))) 
