3e0791e675dd829ee6b7792885e8f6cdc0bc8ede
ray38 <xlei38@gatech.edu>
2018-01-02 04:19:32 -0500

    added fit json

---
+++ psi4_fit_json.py
@@ -0,0 +1,277 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 09:43:05 2017
+
+@author: ray
+"""
+
+import numpy as np
+import sys
+import os
+import time
+import math
+import json
+from glob import glob
+from sklearn import linear_model
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense, Activation
+import keras
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.axes3d as p3
+
+import itertools
+import multiprocessing
+
+try: import cPickle as pickle
+except: import pickle
+import matplotlib.pyplot as plt
+from subsampling import subsampling_system,random_subsampling,subsampling_system_with_PCA
+
+def map_to_n1_1(arr, maxx, minn):
+    return np.divide(np.subtract(arr,minn),(maxx-minn)/2.)-1.
+    
+def log(log_filename, text):
+    with open(log_filename, "a") as myfile:
+        myfile.write(text)
+    return
+
+def map_to_0_1(arr, maxx, minn):
+    return np.divide(np.subtract(arr,minn),(maxx-minn))
+    
+def map_back(arr, maxx, minn):
+    return np.add(np.multiply(arr,(maxx-minn)),minn)
+
+def get_start_loss(log_filename):
+    
+    with open(log_filename, 'r') as f:
+        for line in f:
+            pass
+        temp = line
+    
+    if temp.strip().startswith('updated'):
+        return float(temp.split()[2])
+    else:
+        raise ValueError
+
+def fit_with_Linear(X,y):
+
+    filename = "Linear_model.sav"
+
+    try:
+        li_model = pickle.load(open(filename, 'rb'))
+        print 'model loaded'
+    except:
+        li_model = linear_model.LinearRegression()
+        li_model.fit(X, y)
+        pickle.dump(li_model, open(filename, 'wb'))
+    
+    # The coefficients
+    print 'Coefficients: \n', li_model.coef_
+    # The mean squared error
+    print "Mean squared error: %.20f" % np.mean((li_model.predict(X) - y) ** 2)
+    # Explained variance score: 1 is perfect prediction
+    print 'Variance score: %.20f' % li_model.score(X, y)
+    
+    residual = y-li_model.predict(X)
+#    plt.scatter(X, residual,  color='black')
+#    plt.show()
+    return residual, li_model
+
+def fit_with_KerasNN(X, y, tol, slowdown_factor, early_stop_trials):
+
+
+    filename = "NN.h5"
+    log_filename = "NN_fit_log.log"
+
+    n_layers = setup["NN_setup"]["number_layers"]
+    n_per_layer = setup["NN_setup"]["number_neuron_per_layer"]
+    activation = setup["NN_setup"]["activation"]
+
+
+
+    try:
+        model = load_model(filename)
+        restart = True
+        print 'model loaded: ' + filename
+    except:
+        restart = False
+        n = int(n_per_layer)
+        k = len(X[0])
+        print n,k
+        model = Sequential()
+        model.add(Dense(output_dim =n, input_dim = k, activation = activation))
+    
+        if n_layers > 1:        
+            for i in range(int(n_layers-1)):
+                model.add(Dense(input_dim = n, output_dim  = n, activation = activation))
+        model.add(Dense(input_dim = n,output_dim =1, activation = 'linear'))
+    
+    #    model.add(Dense(input_dim = 1,output_dim =1, activation = 'linear',  init='uniform'))
+    
+    print 'model set'
+    default_lr = 0.001
+    adam = keras.optimizers.Adam(lr=default_lr / slowdown_factor)
+    model.compile(loss='mse',#custom_loss,
+              optimizer=adam)
+              #metrics=['mae'])
+    print model.summary()
+    print model.get_config()
+    
+    est_start = time.time()
+    history_callback = model.fit(X, y, nb_epoch=1, batch_size=50000)
+    est_epoch_time = time.time()-est_start
+    if est_epoch_time >= 30.:
+        num_epoch = 1
+    else:
+        num_epoch = int(math.floor(30./est_epoch_time))
+    if restart == True:
+        try:
+            start_loss = get_start_loss(log_filename)
+        except:
+            loss_history = history_callback.history["loss"]
+            start_loss = np.array(loss_history)[0]
+    else:
+        loss_history = history_callback.history["loss"]
+        start_loss = np.array(loss_history)[0]
+    
+    log(log_filename, "\n start: "+ str(start_loss))
+    
+    old_loss = start_loss
+    keep_going = True
+    
+    count_epochs = 0
+    while keep_going:
+        count_epochs += 1
+        history_callback = model.fit(X, y, nb_epoch=num_epoch, batch_size=50000, shuffle=True)
+        loss_history = history_callback.history["loss"]
+        new_loss = np.array(loss_history)[-1]
+        if new_loss < old_loss:
+            model.save(filename)
+            print 'model saved'
+            log(log_filename, "\n updated best: "+ str(new_loss) + " \t epochs since last update: " + str(count_epochs))
+            old_loss = new_loss
+            count_epochs = 0
+        if new_loss < tol:
+            keep_going = False
+        if count_epochs >=early_stop_trials:
+            keep_going = False
+    
+    return model
+
+
+
+def read_data_from_one_dir(directory):
+    temp_cwd = os.getcwd()
+    os.chdir(directory)
+
+    subsampled_filename = "overall_subsampled_data.p"
+    random_filename = "overall_random_data.p"
+
+    try:
+        molecule_subsampled_data = pickle.load(open(subsampled_filename,'rb'))
+    except:
+        molecule_subsampled_data = []
+
+    try:
+        molecule_random_data = pickle.load(open(random_filename,'rb'))
+    except:
+        molecule_random_data = []
+
+    os.chdir(temp_cwd)
+
+    return molecule_subsampled_data, molecule_random_data
+
+
+
+def get_training_data(dataset_name,setup):
+
+    data_dir_name = setup["working_dir"] + "/data/" 
+    data_paths = glob(data_dir_name)
+
+    overall_subsampled_data = []
+    overall_random_data = []
+    for directory in data_paths:
+        temp_molecule_subsampled_data, temp_molecule_random_data = read_data_from_one_dir(directory)
+        overall_subsampled_data += temp_molecule_subsampled_data
+        overall_random_data += temp_molecule_random_data
+
+
+    overall_random_data = random_subsampling(overall_random_data, setup["random_pick"])
+
+    list_subsample = setup["subsample_feature_list"]
+    temp_list_subsample = setup["subsample_feature_list"]
+    if temp_list_subsample == []:
+        for m in range(len(overall_subsampled_data[0])):
+            temp_list_subsample.append(m)
+
+    if len(temp_list_subsample) <= 10:
+        overall_subsampled_data = subsampling_system(overall_subsampled_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]))
+    else:
+        overall_subsampled_data = subsampling_system_with_PCA(overall_subsampled_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]),start_trial_component = 9)
+
+
+    overall = overall_random_data + overall_subsampled_data
+
+
+
+    X_train = []
+    y_train = []
+    dens = []
+
+    for entry in overall:
+#        if entry[0] >= lower and entry[0] <= upper:
+        X_train.append(list(entry[1:]))
+        dens.append(entry[1])
+        y_train.append(entry[0])
+    
+    
+    X_train = (np.asarray(X_train))
+    y_train = np.asarray(y_train).reshape((len(y_train),1))
+    dens = np.asarray(dens).reshape((len(dens),1))
+    
+    return X_train, y_train, dens
+
+
+
+if __name__ == "__main__":
+
+
+    setup_filename = sys.argv[1]
+    dataset_name = sys.argv[2]
+    slowdown_factor = float(sys.argv[3])
+    tol = float(sys.argv[4])
+    try:
+        early_stop_trials = int(sys.argv[5])
+    except:
+        early_stop_trials = 100
+
+    with open(setup_filename) as f:
+        setup = json.load(f)
+
+
+    h = float(setup['grid_spacing'])
+    L = float(setup['box_dimension'])
+    N = int(setup['number_segment_per_side'])
+    dir_name = "{}_{}_{}".format(str(L).replace('.','-'),str(h).replace('.','-'),N)
+
+    working_dir = os.getcwd() + '/' + dir_name + '/' + setup_name
+    setup["working_dir"] = working_dir
+
+    model_save_dir = working_dir + "NN_linear_residual_{}_{}_{}".format(setup["NN_setup"]["number_neuron_per_layer"], setup["NN_setup"]["number_layers"], setup["NN_setup"]["activation"])
+   
+    setup["model_save_dir"] = model_save_dir
+
+    
+    
+    X_train,y, dens = get_training_data(dataset_name,setup)
+   
+    
+    os.chdir(model_save_dir)
+    
+    residual,li_model = fit_with_Linear(dens,y)
+    model = fit_with_KerasNN(X_train,residual, tol, slowdown_factor, early_stop_trials)
+
+
+    
