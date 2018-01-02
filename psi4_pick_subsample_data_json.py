b52974bcdbf30d8d7c742d3bcad2bf997e48cf1d
ray38 <xlei38@gatech.edu>
2018-01-01 21:12:52 -0500

    updated pick subsample json

--- psi4_pick_subsample_data_json.py
+++ psi4_pick_subsample_data_json.py
@@ -27,17 +27,15 @@ def log(log_filename, text):
 
 
 
-def write_data_to_file2(i,j,k,data,random_data, molecule,functional, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform):
-    temp = "{}_{}_{}_{}_{}_{}_gamma{}_dev{}_devsq{}_inte{}_{}_{}".format(molecule,functional,i,j,k, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform)
+def write_data_to_file2(i,j,k,data):
+    temp = "{}_{}_{}".format(i,j,k)
     with open(temp + "_subsampled_data.p", 'wb') as handle:
         pickle.dump(data, handle, protocol=2)
 
-    with open(temp + "_subsampled_random_data.p", 'wb') as handle:
-        pickle.dump(data + random_data, handle, protocol=2)
     return
 
-def write_data_to_file_random(i,j,k,random_data, molecule,functional, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform):
-    temp = "{}_{}_{}_{}_{}_{}_gamma{}_dev{}_devsq{}_inte{}_{}_{}".format(molecule,functional,i,j,k, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform)
+def write_data_to_file_random(i,j,k,random_data):
+    temp = "{}_{}_{}".format(i,j,k)
     
     with open(temp + "_random_data.p", 'wb') as handle:
         pickle.dump(random_data, handle, protocol=2)
@@ -46,32 +44,32 @@ def write_data_to_file_random(i,j,k,random_data, molecule,functional, target, ga
 
 
 
-def process_data(i,j,k,setup):
-    random_pick_number = int(math.ceil((float(len(processed_data)) * random_rate)))
+def process_data(i,j,k,log_filename,setup):
+    random_pick_number = int(math.ceil((float(len(processed_data)) * float(setup["random_pick_rate"]))))
     temp_random_data = random_subsampling(processed_data, random_pick_number)
-    write_data_to_file_random(i,j,k,temp_random_data, molecule,functional, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform)
+    write_data_to_file_random(i,j,k,temp_random_data)
     
     log(log_filename,"\nstart sub-sampling") 
     sample_start = time.time() 
     log(log_filename,"\nmolecule length before: " + str(len(processed_data)))
     if len(list_subsample) <= 10:
-        processed_data = subsampling_system(processed_data, list_desc = list_subsample, cutoff_sig = 0.01, rate = 0.2)
+        processed_data = subsampling_system(processed_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]))
     else:
-        processed_data = subsampling_system_with_PCA(processed_data, list_desc = list_subsample, cutoff_sig = 0.01, rate = 0.2,start_trial_component = 9)
+        processed_data = subsampling_system_with_PCA(processed_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]),start_trial_component = 9)
     log(log_filename,"\nmolecule length after: " + str(len(processed_data)))  
     log(log_filename,"\nfinished sampling, took: " + str(time.time()-sample_start))
-    write_data_to_file2(i,j,k,processed_data, temp_random_data, molecule,functional, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform)
+    write_data_to_file2(i,j,k,processed_data)
     
     return
     
     
 def subsample_one_molecule(data,i,j,k,molecule,functional, setup):
-    log_filename = "{}_{}_{}_{}_{}_{}_gamma{}_dev{}_devsq{}_inte{}_{}_{}_subsample_log.log".format(molecule,functional,i,j,k, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform)
+    log_filename = "{}_{}_{}_subsample_log.log".format(i,j,k)
     with open(log_filename, "w") as myfile:
         myfile.write('')
     log(log_filename,"\nstart reading: " + molecule) 
     print "start reading: " + molecule
-    process_data(i,j,k,setup)
+    process_data(i,j,k,log_filename,setup)
     return
 
 
@@ -85,7 +83,7 @@ def transform_data(temp_data, transform):
     elif transform == "neglog10":
         return np.log10(np.multiply(-1., temp_data.flatten())).tolist()
 
-def process_each_block(molecule,functional,i,j,k, setup):
+def process_each_block(molecule,functional,i,j,k, setup, data_dir_full):
     data_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
     print data_filename
     data =  h5py.File(data_filename,'r')
@@ -153,18 +151,19 @@ def process_each_block(molecule,functional,i,j,k, setup):
 
     
     result = zip(*result_list)
-    
-    parent = os.getcwd()
-    dir_name = "{}_{}_gamma{}_dev{}_devsq{}_inte{}_{}_{}_tau".format(functional, target, gamma,num_desc_deri,num_desc_deri_squa,num_desc_ave_dens,desc_transform,target_transform)    
-    if os.path.isdir(dir_name) == False:
-        os.makedirs(parent + '/' + dir_name)      
-    os.chdir(parent + '/' + dir_name)
-    
     print "done picking: {} {} {}".format(i,j,k)
+    
+
+    molecule_dir = setup["working_dir"] + '/' + molecule
+    if os.path.isdir(molecule_dir) == False:
+        os.makedirs(molecule_dir)
+    os.chdir(molecule_dir)
+    
+    
     subsample_one_molecule(result,i,j,k,molecule,functional, target, gamma, num_desc_deri, num_desc_deri_squa, num_desc_ave_dens,desc_transform,target_transform)
 
     
-    os.chdir(parent)
+    os.chdir(data_dir_full)
     return
 
 
@@ -172,17 +171,18 @@ def process_each_block(molecule,functional,i,j,k, setup):
         
         
 def process_one_molecule(molecule, functional,h,L,N, setup):
-    cwd = os.getcwd()
     database_name = setup["database_directory"]
     sub_database_name = "{}_{}_{}".format(str(L).replace('.','-'),str(h).replace('.','-'),N)
-    dir_name = "{}_{}_{}_{}_{}".format(molecule,functional,str(L).replace('.','-'),str(h).replace('.','-'),N)
-    print dir_name
+    data_dir_name = "{}_{}_{}_{}_{}".format(molecule,functional,str(L).replace('.','-'),str(h).replace('.','-'),N)
+
+    data_dir_full = database_directory + '/' + sub_database_name + '/' + dir_name
+    print data_dir_full
     
-    if os.path.isdir(database_directory + '/' + sub_database_name + '/' + dir_name) == False:
+    if os.path.isdir(data_dir_full) == False:
         print '\n****Error: Cant find the database directory! ****\n'
         raise NotImplementedError
     
-    os.chdir(database_directory + '/' + sub_database_name + '/' + dir_name)
+    os.chdir(data_dir_full)
     
     Nx = Ny = Nz = N
 #    for i in range(Nx):
@@ -201,7 +201,7 @@ def process_one_molecule(molecule, functional,h,L,N, setup):
     
     pool = multiprocessing.Pool()
     for i,j,k in paramlist:
-        pool.apply_async(process_each_block, args=(molecule,functional,i,j,k, setup))
+        pool.apply_async(process_each_block, args=(molecule,functional,i,j,k, setup, data_dir_full))
     pool.close()
     pool.join()
 
@@ -212,17 +212,26 @@ def process_one_molecule(molecule, functional,h,L,N, setup):
 
 if __name__ == "__main__":
 
-    setup_filename = sys.argv[1]
-    choice = sys.argv[2]
+    setup_database_filename = sys.argv[1]
+    setup_name = sys.argv[2]
+    choice = sys.argv[3]
 
     if choice not in ['single','set']:
         raise NotImplementedError
 
-    with open(setup_filename) as f:
-        setup = json.load(f)
+    with open(setup_database_filename) as f:
+        setup_database = json.load(f)
+
+    setup = setup_database[setup_name]
+    working_dir = os.getcwd() + '/' + setup_name
+
+    if os.path.isdir(working_dir) == False:
+        os.makedirs(working_dir)    
+
+    setup["working_dir"] = working_dir
     
     if choice == 'single':
-        molecule = sys.argv[3]
+        molecule = sys.argv[4]
         h = float(setup['grid_spacing'])
         L = float(setup['box_dimension'])
         N = int(setup['number_segment_per_side'])
