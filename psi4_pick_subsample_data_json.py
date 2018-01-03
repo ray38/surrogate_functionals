e9564aaa030413716aa3a295927e5185d946945b
ray38 <xlei38@gatech.edu>
2018-01-03 16:57:35 -0500

    added check for subsampling

--- psi4_pick_subsample_data_json.py
+++ psi4_pick_subsample_data_json.py
@@ -96,95 +96,111 @@ def transform_data(temp_data, transform):
 
 def process_each_block(molecule,functional,i,j,k, setup, data_dir_full):
     data_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
-    print data_filename
-    data =  h5py.File(data_filename,'r')
-    
+    molecule_dir = setup["working_dir"] + '/data/' + molecule
 
-    result_list = []
-    
-    target = setup['target']
-    
-    if target == 'Vxc':
-        target_set_name = 'V_xc'   
-    if target == 'epxc':
-        target_set_name = 'epsilon_xc'
-    if target == 'tau':
-        target_set_name = 'tau'
-    if target == 'gamma':
-        target_set_name = 'gamma'
-    
-    temp_data = np.asarray(data[target_set_name])
-#    print np.sum(np.sort(temp_data))*0.1*0.1*0.1
-    result_list.append(transform_data(temp_data, setup['target_transform']))
-    
-    if int(setup['density']) == 1:
-        temp_data = np.asarray(data['rho'])
-        result_list.append(transform_data(temp_data, setup['density_transform']))
+    try:
+        os.chdir(molecule_dir)
+        temp_log_filename = "{}_{}_{}_subsample_log.log".format(i,j,k)
+        temp_random_data_filename = "{}_{}_{}_random_data.p".format(i,j,k)
+        temp_subsampled_data_filename = "{}_{}_{}_subsampled_data.p".format(i,j,k)
+        if os.path.isfile(temp_log_filename) and os.path.isfile(temp_random_data_filename) and os.path.isfile(temp_subsampled_data_filename):
+            os.chdir(data_dir_full)
+            return
+        else:
+            raise SystemError
 
-    if int(setup['gamma']) == 1:
-        temp_data = np.asarray(data['gamma'])
-        result_list.append(transform_data(temp_data, setup['gamma_transform']))
+    except:
+        os.chdir(data_dir_full)
 
-    if int(setup['tau']) == 1:
-        temp_data = np.asarray(data['tau'])
-        result_list.append(transform_data(temp_data, setup['tau_transform']))
-
-
-    group_name = 'derivative'
-    temp_list = setup["derivative_list"]
-    if len(temp_list) > 0: 
-        for derivative_count in temp_list:
-            dataset_name = 'derivative_{}'.format(derivative_count)
-            temp_data = np.asarray(data[group_name][dataset_name])
-            result_list.append(transform_data(temp_data, setup['derivative_transform']))
-
-    temp_list = setup["derivative_square_list"]
-    if len(temp_list) > 0: 
-        for derivative_count in temp_list:
-            dataset_name = 'derivative_{}'.format(derivative_count)
-            temp_data = np.power(np.asarray(data[group_name][dataset_name]), 2.)
-            result_list.append(transform_data(temp_data, setup['derivative_square_transform']))
-    
-   
-    group_name = 'average_density'
-    temp_list = setup["average_density_r_list"]
-    if len(temp_list) > 0:
-        for r_list_count in temp_list:
-            dataset_name = 'average_density_{}'.format(str(r_list_count).replace('.','-'))
-            temp_data = np.asarray(data[group_name][dataset_name])
-            result_list.append(transform_data(temp_data, setup['average_density_transform']))
-
-
-    group_name = 'asym_integral'
-    temp_list = setup["asym_desc_r_list"]
-    if len(temp_list) > 0:
-        for r_list_count in temp_list:
-            dataset_name = 'asym_integral_x_{}'.format(str(r_list_count).replace('.','-'))
-            temp_data = np.asarray(data[group_name][dataset_name])
-            result_list.append(transform_data(temp_data, setup['asym_desc_transform']))
-
-            dataset_name = 'asym_integral_y_{}'.format(str(r_list_count).replace('.','-'))
-            temp_data = np.asarray(data[group_name][dataset_name])
-            result_list.append(transform_data(temp_data, setup['asym_desc_transform']))
-
-            dataset_name = 'asym_integral_z_{}'.format(str(r_list_count).replace('.','-'))
-            temp_data = np.asarray(data[group_name][dataset_name])
-            result_list.append(transform_data(temp_data, setup['asym_desc_transform']))
-
-    
-    result = zip(*result_list)
-    print "done picking: {} {} {}".format(i,j,k)
-    
-
-    molecule_dir = setup["working_dir"] + '/' + molecule
-    if os.path.isdir(molecule_dir) == False:
-        os.makedirs(molecule_dir)
-    os.chdir(molecule_dir)
+        print data_filename
+        data =  h5py.File(data_filename,'r')
         
-    subsample_one_molecule(result,i,j,k,setup)
 
-    os.chdir(data_dir_full)
-    return
+        result_list = []
+        
+        target = setup['target']
+        
+        if target == 'Vxc':
+            target_set_name = 'V_xc'   
+        if target == 'epxc':
+            target_set_name = 'epsilon_xc'
+        if target == 'tau':
+            target_set_name = 'tau'
+        if target == 'gamma':
+            target_set_name = 'gamma'
+        
+        temp_data = np.asarray(data[target_set_name])
+    #    print np.sum(np.sort(temp_data))*0.1*0.1*0.1
+        result_list.append(transform_data(temp_data, setup['target_transform']))
+        
+        if int(setup['density']) == 1:
+            temp_data = np.asarray(data['rho'])
+            result_list.append(transform_data(temp_data, setup['density_transform']))
+
+        if int(setup['gamma']) == 1:
+            temp_data = np.asarray(data['gamma'])
+            result_list.append(transform_data(temp_data, setup['gamma_transform']))
+
+        if int(setup['tau']) == 1:
+            temp_data = np.asarray(data['tau'])
+            result_list.append(transform_data(temp_data, setup['tau_transform']))
+
+
+        group_name = 'derivative'
+        temp_list = setup["derivative_list"]
+        if len(temp_list) > 0: 
+            for derivative_count in temp_list:
+                dataset_name = 'derivative_{}'.format(derivative_count)
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, setup['derivative_transform']))
+
+        temp_list = setup["derivative_square_list"]
+        if len(temp_list) > 0: 
+            for derivative_count in temp_list:
+                dataset_name = 'derivative_{}'.format(derivative_count)
+                temp_data = np.power(np.asarray(data[group_name][dataset_name]), 2.)
+                result_list.append(transform_data(temp_data, setup['derivative_square_transform']))
+        
+       
+        group_name = 'average_density'
+        temp_list = setup["average_density_r_list"]
+        if len(temp_list) > 0:
+            for r_list_count in temp_list:
+                dataset_name = 'average_density_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, setup['average_density_transform']))
+
+
+        group_name = 'asym_integral'
+        temp_list = setup["asym_desc_r_list"]
+        if len(temp_list) > 0:
+            for r_list_count in temp_list:
+                dataset_name = 'asym_integral_x_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, setup['asym_desc_transform']))
+
+                dataset_name = 'asym_integral_y_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, setup['asym_desc_transform']))
+
+                dataset_name = 'asym_integral_z_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, setup['asym_desc_transform']))
+
+        
+        result = zip(*result_list)
+        print "done picking: {} {} {}".format(i,j,k)
+        
+
+    #    molecule_dir = setup["working_dir"] + '/data/' + molecule
+        if os.path.isdir(molecule_dir) == False:
+            os.makedirs(molecule_dir)
+        os.chdir(molecule_dir)
+            
+        subsample_one_molecule(result,i,j,k,setup)
+
+        os.chdir(data_dir_full)
+        return
 
 
 
