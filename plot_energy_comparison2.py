1f0deddb9eeb9d0bb3a8162cd7a2623e9bc6cf00
ray38 <xlei38@gatech.edu>
2018-05-23 11:38:47 -0400

    test

---
+++ plot_energy_comparison2.py
@@ -0,0 +1,31 @@
+import os
+
+import sys
+import csv
+try: import cPickle as pickle
+except: import pickle
+import time
+import pprint
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+if __name__ == "__main__":
+
+	filename = sys.argv[1]
+
+	with open('filename.pickle', 'rb') as handle:
+    	data = pickle.load(handle)
+
+
+	sns.set(style="whitegrid", palette="pastel", color_codes=True)
+
+    # Draw a nested violinplot and split the violins for easier comparison
+    sns.violinplot(x="model_name", y="formation_exc_error", hue="training_test", data=data, split=True, inner="quartile", palette={"training": "b", "test": "y"})
+    sns.despine(left=True)
+
+    plt.savefig("formation_energy_grouped_violin_plot.png")
