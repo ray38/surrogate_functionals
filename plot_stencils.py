e3b188d8e95a6842db0f4ca64d877732d8367979
ray38 <xlei38@gatech.edu>
2018-05-24 12:26:48 -0400

    added plot stencil

--- plot_stencils.py
+++ plot_stencils.py
@@ -20,22 +20,27 @@ def pad(array, reference_shape):
 	result[x_offset: array_shape[0] + x_offset, y_offset: array_shape[1] + y_offset] = array
 
 	return result
-'''
+
 h = 0.02
 r_list = [0.6, 0.1, 0.14]
 stencil_list = []
+
+stencil_dict = {}
 pad_list = []
 for r in r_list:
     temp_stencil,temp_pad = get_integration_stencil(h, h, h, r, accuracy = get_auto_accuracy(h,h,h, r))
     stencil_list.append(temp_stencil[(temp_stencil.shape[1]-1)/2])
+    stencil_dict[r] = temp_stencil[(temp_stencil.shape[1]-1)/2]
     pad_list.append(temp_pad)
 
 
-for stencil in stencil_list:
+for key in stencil_dict:
 	plt.figure()
-	sns.heatmap(stencil, cmap="PiYG")
-'''
+	sns.heatmap(pad(stencil_dict[r],21), cmap="RdBu_r")
+	plt.savefig("integration_stencil_plot_{}.png".format(r))
 
+
+'''
 test = np.random.rand(3,3)
 
 sns.heatmap(test, cmap="PiYG", center=0.)
@@ -46,3 +51,4 @@ sns.plt.show()
 
 sns.heatmap(pad(test,21), cmap="PiYG", center=0.)
 sns.plt.show()
+'''
