0d11e48a76ed83b7bd5d043901ad4a4d14096bc9
ray38 <xlei38@gatech.edu>
2018-05-29 12:54:04 -0400

    add pca

--- psi4_fit_pca.py
+++ psi4_fit_pca.py
@@ -218,11 +218,11 @@ def read_data_from_one_dir(directory):
 
 def get_training_data(dataset_name,setup):
 
-    #data_dir_name = setup["working_dir"] + "/data/*/" 
-    #data_paths = glob(data_dir_name)
-    #print data_paths
+    data_dir_name = setup["working_dir"] + "/data/*/" 
+    data_paths = glob(data_dir_name)
+    print data_paths
 
-    data_paths = ["/gpfs/pace1/project/chbe-medford/medford-share/users/xlei38/psi4_feature_picked_database/B3LYP_float64_test/10-0_0-02_5/epxc_mGGA_allrange_real_real_numerical/data/C2H6/", "/gpfs/pace1/project/chbe-medford/medford-share/users/xlei38/psi4_feature_picked_database/B3LYP_float64_test/10-0_0-02_5/epxc_mGGA_allrange_real_real_numerical/data/NH3/"]
+    #data_paths = ["/gpfs/pace1/project/chbe-medford/medford-share/users/xlei38/psi4_feature_picked_database/B3LYP_float64_test/10-0_0-02_5/epxc_mGGA_allrange_real_real_numerical/data/C2H6/", "/gpfs/pace1/project/chbe-medford/medford-share/users/xlei38/psi4_feature_picked_database/B3LYP_float64_test/10-0_0-02_5/epxc_mGGA_allrange_real_real_numerical/data/NH3/"]
     overall_subsampled_data = []
     overall_random_data = []
     overall_molecule_name_list_subsampled = []
