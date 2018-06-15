facd73b608499b5bd6c86fdc1ec532d78eaa274d
ray38 <xlei38@gatech.edu>
2018-06-15 12:11:11 -0400

    test

--- get_test_data_for_plot2.py
+++ get_test_data_for_plot2.py
@@ -103,7 +103,7 @@ def get_training_data(dataset_name,setup, cutoff_sig):
         #overall_random_data += random_subsampling(temp_molecule_random_data, num_random_per_molecule)
 
 
-    overall_subsampled_data = subsampling_system_with_PCA(overall_subsampled_data, list_desc = [], cutoff_sig = cutoff_sig, rate = 0.05,start_trial_component = 9)
+    overall_subsampled_data = subsampling_system_with_PCA(overall_subsampled_data, list_desc = [], cutoff_sig = cutoff_sig, rate = 0.1,start_trial_component = 9)
     #overall_subsampled_data = subsampling_system(overall_subsampled_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]))
     overall = np.asarray(overall_subsampled_data)
 
