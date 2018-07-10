0a28134ddbe42c541583f4890176647c8c4c1a2a
ray38 <xlei38@gatech.edu>
2018-07-09 21:46:23 -0400

    test

--- getDescriptors_json_sequential_copy.py
+++ getDescriptors_json_sequential_copy.py
@@ -214,10 +214,10 @@ def process_one_molecule(molecule, functional,h,L,N,r_list,asym_list):
 
 
 
-    #for i,j,k in paramlist:
-    #    process(molecule, functional,i,j,k,h,N,r_list,asym_list)
+    for i,j,k in paramlist:
+        process(molecule, functional,i,j,k,h,N,r_list,asym_list)
         
-    process(molecule, functional,0,0,0,h,N,r_list,asym_list)
+    #process(molecule, functional,0,0,0,h,N,r_list,asym_list)
     
     os.chdir(cwd)
     return
