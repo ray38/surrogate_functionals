97705a6af4684d198bdec62c2f1340793169bafa
ray38 <xlei38@gatech.edu>
2018-07-13 13:05:02 -0400

    test

--- test_rotational_invariance_harmonic.py
+++ test_rotational_invariance_harmonic.py
@@ -241,7 +241,7 @@ for r in [0.02, 0.06, 0.10, 0.14, 0.18, 0.22]:
 			theta2_list.append(str(theta2))
 			theta3_list.append(str(theta3))
 
-d = {"r":r_list, "m":m_list, "l":l_list, "error":result_error_list, "percent_error":result_percent_error_list,"theta1":theta1_list, "theta2":theta2_list, "theta3": theta3_list}
+d = {"r":r_list, "m":m_list, "l":l_list, "error":result_error_list, "percent_error":result_percent_error_list,"log_error": np.log10(np.abs(result_error_list)).tolist(), "log_percent_error": np.log10(np.abs(result_percent_error_list)).tolist(), "theta1":theta1_list, "theta2":theta2_list, "theta3": theta3_list}
 data = pd.DataFrame(data=d)
 plt.figure()
 	
