86db89c64cc96820f2bae3d86bd6c4869ce54c7b
ray38 <xlei38@gatech.edu>
2018-07-13 13:37:24 -0400

    test

--- test_rotational_invariance_harmonic2.py
+++ test_rotational_invariance_harmonic2.py
@@ -159,16 +159,59 @@ def get_result(x_temp, y_temp, z_temp,sig_x,sig_y,sig_z,x0,y0,z0, h, stencil, pa
 	return temp[(temp.shape[0]-1)/2, (temp.shape[1]-1)/2, (temp.shape[2]-1)/2]
 
 
+def plot_result(data):
+
+	plt.figure()
+		
+	sns.set(style="whitegrid", palette="pastel", color_codes=True)
+
+	ax = sns.violinplot(x = "r",hue="m",y="percent_error",data=data)
+	plt.tight_layout()
+	plt.savefig("harmonic_rotational_invariance_test_percent_error.png")
+
+
+	plt.figure()
+		
+	sns.set(style="whitegrid", palette="pastel", color_codes=True)
+
+	ax = sns.violinplot(x = "r",hue="m",y="log_error",data=data)
+	plt.tight_layout()
+	plt.savefig("harmonic_rotational_invariance_test_error.png")
+
+
+	plt.figure()
+		
+	sns.set(style="whitegrid", palette="pastel", color_codes=True)
+
+	ax = sns.violinplot(x = "r",hue="m",y="log_percent_error",data=data)
+	plt.tight_layout()
+	plt.savefig("harmonic_rotational_invariance_test_percent_error_log.png")
+
+
+	plt.figure()
+		
+	sns.set(style="whitegrid", palette="pastel", color_codes=True)
+
+	ax = sns.violinplot(x = "r",hue="m",y="error",data=data)
+	plt.tight_layout()
+	plt.savefig("harmonic_rotational_invariance_test_error_log.png")
+
+	return
+
 #r = float(sys.argv[1])
 num_rot = int(sys.argv[1])
 num_grid = int(sys.argv[2])
 nx, ny, nz = (25,25,25)
 h = 0.02
 
+temp_x = (float(nx)-1.0)*h/2.0
+temp_y = (float(ny)-1.0)*h/2.0
+temp_z = (float(nz)-1.0)*h/2.0
 
-xv = np.linspace(-1.0,1.0,nx)
-yv = np.linspace(-1.0,1.0,ny)
-zv = np.linspace(-1.0,1.0,nz)
+
+xv = np.linspace(-temp_x,temp_x,nx)
+yv = np.linspace(-temp_y,temp_y,ny)
+zv = np.linspace(-temp_z,temp_z,nz)
 
 x, y, z = np.meshgrid(xv, yv, zv)
 #x,y,z = rotate_coord_mat2(x1,y1,z1)
@@ -204,7 +247,9 @@ temp_z0_list = np.linspace(-0.5, 0.5, num_grid)
 origin_list = list(itertools.product(temp_x0_list,temp_y0_list,temp_z0_list))
 
 
+counter_origin = 0
 for x0, y0, z0 in origin_list:
+	counter_origin +=1 
 	for r in [0.02, 0.06, 0.10, 0.14, 0.18, 0.22]:
 	#	for l, m in [(1, -1), (1, 0), (1, 1),(1,"sum")]:
 		for l, m in [(1,"sum")]:
@@ -239,7 +284,7 @@ for x0, y0, z0 in origin_list:
 
 				x_temp, y_temp, z_temp = rotate_coord_mat2(x.copy(),y.copy(),z.copy(),theta1,theta2,theta3)
 				error = get_result(x_temp, y_temp, z_temp,sig_x,sig_y,sig_z,x0,y0,z0, h, stencil_Re, pad) - truth
-				print "{}\t{}\t{}\t{}\t{}\t{}\t{}".format(r, counter,l, m,error/truth, truth, error)
+				print "origin:{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(counter_origin,r, counter,l, m,error/truth, truth, error)
 				result_percent_error_list.append(error/truth)
 				result_error_list.append(error)
 				r_list.append(str(r))
@@ -249,39 +294,6 @@ for x0, y0, z0 in origin_list:
 				theta2_list.append(str(theta2))
 				theta3_list.append(str(theta3))
 
-d = {"r":r_list, "m":m_list, "l":l_list, "error":result_error_list, "percent_error":result_percent_error_list,"log_error": np.log10(np.abs(result_error_list)).tolist(), "log_percent_error": np.log10(np.abs(result_percent_error_list)).tolist(), "theta1":theta1_list, "theta2":theta2_list, "theta3": theta3_list}
-data = pd.DataFrame(data=d)
-plt.figure()
-	
-sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
-ax = sns.violinplot(x = "r",hue="m",y="percent_error",data=data)
-plt.tight_layout()
-plt.savefig("rotational_invariance_test_percent_error.png")
-
-
-plt.figure()
-	
-sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
-ax = sns.violinplot(x = "r",hue="m",y="log_error",data=data)
-plt.tight_layout()
-plt.savefig("rotational_invariance_test_error.png")
-
-
-plt.figure()
-	
-sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
-ax = sns.violinplot(x = "r",hue="m",y="log_percent_error",data=data)
-plt.tight_layout()
-plt.savefig("rotational_invariance_test_percent_error_log.png")
-
-
-plt.figure()
-	
-sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
-ax = sns.violinplot(x = "r",hue="m",y="error",data=data)
-plt.tight_layout()
-plt.savefig("rotational_invariance_test_error_log.png")
+	d = {"r":r_list, "m":m_list, "l":l_list, "error":result_error_list, "percent_error":result_percent_error_list,"log_error": np.log10(np.abs(result_error_list)).tolist(), "log_percent_error": np.log10(np.abs(result_percent_error_list)).tolist(), "theta1":theta1_list, "theta2":theta2_list, "theta3": theta3_list}
+	data = pd.DataFrame(data=d)
+	plot_result(data)
