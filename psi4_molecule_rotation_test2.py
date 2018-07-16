b741a4c03665bd58f3534df49977d89539fe4d57
ray38 <xlei38@gatech.edu>
2018-07-16 14:57:12 -0400

    test

--- psi4_molecule_rotation_test2.py
+++ psi4_molecule_rotation_test2.py
@@ -296,7 +296,7 @@ def plot_result(data):
 
     #ax = sns.violinplot(x = "Molecule",y="Value",row="Type", col="Property",data=data)
     
-    ax = sns.factorplot(x = "Molecule",y="Value",row="Type", col="Property",data=data, kind="violin", split=True)
+    ax = sns.factorplot(x = "Molecule",y="Value",row="Type", col="Property",data=data, kind="violin", split=True,sharey = False)
 
     plt.tight_layout()
     plt.savefig("Molecule_rotational_invariance_test.png")
