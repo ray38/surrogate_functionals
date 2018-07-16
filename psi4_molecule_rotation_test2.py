0858889ed70df39a47dead66879e955c0e7fd1f4
ray38 <xlei38@gatech.edu>
2018-07-16 15:04:50 -0400

    test

--- psi4_molecule_rotation_test2.py
+++ psi4_molecule_rotation_test2.py
@@ -216,7 +216,7 @@ def process_system(molecule, molecule_name, xc, h, cell, num_blocks, psi4_option
     cwd = os.getcwd()
     
     if psi4_options == None:
-        psi4_options = {"BASIS": "aug-cc-pvtz",
+        psi4_options = {"BASIS": "cc-pvdz",
                     "D_CONVERGENCE":1e-12,
                     "E_CONVERGENCE":1e-12,
                   'DFT_BLOCK_MAX_POINTS': 500000,
