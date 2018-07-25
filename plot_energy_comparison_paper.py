b310bc3503661b71d63ecca36e50331d1c621346
ray38 <xlei38@gatech.edu>
2018-07-25 16:24:16 -0400

    test

--- plot_energy_comparison_paper.py
+++ plot_energy_comparison_paper.py
@@ -69,15 +69,20 @@ if __name__ == "__main__":
 	ax2 = sns.swarmplot(x="model_name",y="formation_exc_error",data=data, hue='training_test',order = order, split=True, color='k', alpha=0.7, palette=("Dark2"))
 	ax1.set_xticklabels(ax.get_xticklabels(),rotation=45)
 	ax2.set_xticklabels(ax.get_xticklabels(),rotation=45)
-	plt.xlabel("Model", fontsize=18)
-	plt.ylabel("Error in Absolute Energy (eV)", fontsize=18)
-	plt.tick_params('both',labelsize='15')
+	plt.xlabel("Model", fontsize=25)
+	plt.ylabel("Error in Formation Energy (eV)", fontsize=25)
+	plt.tick_params('both',labelsize='25')
 
 	handles1, _ = ax1.get_legend_handles_labels()
-	ax1.legend(handles1, ["Training set", "Test set"],fontsize=15)
+	ax1.legend(handles1, ["Training set", "Test set"],fontsize=20,bbox_to_anchor=(0.6, 0.4), loc=2)
 
 	plt.tight_layout()
 	plt.savefig("formation_energy_violin_swarm_plot2.png")
+
+
+
+
+
 
 
 	plt.figure()
@@ -89,12 +94,12 @@ if __name__ == "__main__":
 	ax2 = sns.swarmplot(x="model_name",y="exc_error",data=data, hue='training_test',order = order, split=True, color='k', alpha=0.7, palette=("Dark2"))
 	ax1.set_xticklabels(ax.get_xticklabels(),rotation=45)
 	ax2.set_xticklabels(ax.get_xticklabels(),rotation=45)
-	plt.xlabel("Model", fontsize=18)
-	plt.ylabel("Error in Absolute Energy (eV)", fontsize=18)
-	plt.tick_params('both',labelsize='15')
+	plt.xlabel("Model", fontsize=25)
+	plt.ylabel("Error in Absolute Energy (eV)", fontsize=25)
+	plt.tick_params('both',labelsize='25')
 
 	handles1, _ = ax1.get_legend_handles_labels()
-	ax1.legend(handles1, ["Training set", "Test set"],fontsize=15)
+	ax1.legend(handles1, ["Training set", "Test set"],fontsize=20,bbox_to_anchor=(0.6, 0.4), loc=2)
 
 	plt.tight_layout()
 	plt.savefig("energy_violin_swarm_plot2.png")
@@ -115,32 +120,40 @@ if __name__ == "__main__":
 
 
 
+
+
+
+
 	plt.figure()
 	sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
+	sns.set_context("poster")
 	g = sns.factorplot(x="molecule_name", y="formation_exc_error", hue="model_name",hue_order = order, data=data,
                    capsize=.2, palette="YlGnBu_d", size=12, legend=False, order = ["C2H2","C2H4","C2H6","CH3OH","CH4","CO","CO2","H2","H2O","HCN","HNC","N2","N2O","NH3","O3","CH3CN","CH3CHO","CH3NO2","glycine","H2CCO","H2CO","H2O2","HCOOH","N2H4","NCCN"])
-        g.set_xticklabels(rotation=45,fontsize=15)
-        g.set_yticklabels(fontsize=15)
+        g.set_xticklabels(rotation=90,fontsize=30)
+        g.set_yticklabels(fontsize=40)
 	sns.despine(left=True)
 	#plt.legend(bbox_to_anchor=(1, 1), loc=2)
-	plt.ylabel("Error in Formation Energy (eV)", fontsize=18)
-	plt.xlabel("", fontsize=18)
+	plt.legend(bbox_to_anchor=(0, 0.4), loc=2,fontsize=25)
+	plt.ylabel("Error in Formation Energy (eV)", fontsize=40)
+	plt.xlabel("", fontsize=40)
 	#g.legend( ["Model 1", "Model 2", "Model 3", "Model 4", "Model 5"],loc='upper right',fontsize=15)
 	plt.tight_layout()
 	plt.savefig("formation_factor_plot.png")
 
 
+
+
 	plt.figure()
 	sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
+	sns.set_context("poster")
 	g = sns.factorplot(x="molecule_name", y="exc_error", hue="model_name",hue_order = order, data=data,
                    capsize=.2, palette="YlGnBu_d", size=12, legend=False, order = ["C2H2","C2H4","C2H6","CH3OH","CH4","CO","CO2","H2","H2O","HCN","HNC","N2","N2O","NH3","O3","CH3CN","CH3CHO","CH3NO2","glycine","H2CCO","H2CO","H2O2","HCOOH","N2H4","NCCN"])
-        g.set_xticklabels(rotation=45,fontsize=15)
-        g.set_yticklabels(fontsize=15)
+        g.set_xticklabels(rotation=90,fontsize=30)
+        g.set_yticklabels(fontsize=40)
 	sns.despine(left=True)
 	#plt.legend(bbox_to_anchor=(1, 1), loc=2)
-	plt.ylabel("Error in Absolute Energy (eV)", fontsize=18)
+	plt.legend(bbox_to_anchor=(0, 0.4), loc=2,fontsize=25)
+	plt.ylabel("Error in Absolute Energy (eV)", fontsize=40)
 	plt.xlabel("", fontsize=18)
 	plt.tight_layout()
 	plt.savefig("energy_factor_plot.png")
