57faeb3a5a81fec91f585f4c81378d0055b82748
ray38 <xlei38@gatech.edu>
2018-08-21 09:53:34 -0400

    test

--- plot_energy_comparison_SI.py
+++ plot_energy_comparison_SI.py
@@ -59,8 +59,8 @@ if __name__ == "__main__":
 
 	groups = data.groupby("model_name")
 	for name, group in groups:
-		energy_text_dict[name] = "MAE:\n{:6.2f}\n\nRMSE:\n{:6.2f}".format(np.mean(np.abs(group['exc_error'])), np.sqrt(np.mean(np.square(group['exc_error']))))
-		formation_energy_text_dict[name] = "MAE:\n{:6.2f}\n\nRMSE:\n{:6.2f}".format(np.mean(np.abs(group['formation_exc_error'])), np.sqrt(np.mean(np.square(group['formation_exc_error']))))
+		energy_text_dict[name] = "MAE:\n{:6.2f}\nRMSE:\n{:6.2f}".format(np.mean(np.abs(group['exc_error'])), np.sqrt(np.mean(np.square(group['exc_error']))))
+		formation_energy_text_dict[name] = "MAE:\n{:6.2f}\nRMSE:\n{:6.2f}".format(np.mean(np.abs(group['formation_exc_error'])), np.sqrt(np.mean(np.square(group['formation_exc_error']))))
 
 	energy_text_list = []
 	formation_energy_text_list = []
