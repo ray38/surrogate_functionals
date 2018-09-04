4d48ee9cc8828d66972d4d0603dbbbf3a093dbe2
ray38 <xlei38@gatech.edu>
2018-09-04 16:57:37 -0400

    test

---
+++ model_test/calculate_descriptors_pure_density.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 15:53:10 2017
+
+@author: ray
+"""
+
+import numpy as np
+import sys
+import math
+
+from convolutions import get_integration_stencil,get_auto_accuracy,get_fftconv_with_known_stencil_no_wrap,calc_MC_surface_harmonic_stencil
+import h5py
+import os
+#from joblib import Parallel, delayed
+#import multiprocessing
+import itertools
+import json
+import time
+
+
+def read_system(mol,xc,Nx_center,Ny_center,Nz_center,Nx,Ny,Nz,data_name):
+    def get_index(center,maxx,minn):
+        if center > maxx or center < minn:
+            raise NotImplementedError
+        temp_first = center - 1
+        if temp_first <0:
+            temp_first = maxx
+        temp_second = center
+        temp_third = center +1
+        if temp_third >maxx:
+            temp_third = minn
+        return [temp_first,temp_second,temp_third]
+    
+    Nx_max = Nx-1
+    Ny_max = Ny-1
+    Nz_max = Nz-1
+    Nx_min = 0
+    Ny_min = 0
+    Nz_min = 0
+    
+    Nx_list = get_index(Nx_center,Nx_max,Nx_min)
+    Ny_list = get_index(Ny_center,Ny_max,Ny_min)
+    Nz_list = get_index(Nz_center,Nz_max,Nz_min)
+    
+    temp_x = None
+    x_start = True
+    print Nx_list, Ny_list,Nz_list
+    for i in Nx_list:
+        
+        temp_y = None
+        y_start = True
+        for j in Ny_list:
+            
+            temp_z = None
+            z_start = True
+            for k in Nz_list:
+                
+                temp_filename =  '{}_{}_{}_{}_{}.hdf5'.format(mol,xc,i,j,k)            
+                if os.path.isfile(temp_filename):
+                    raw_data =  h5py.File(temp_filename,'r')
+                    temp_data = np.asarray(raw_data[data_name])
+                if z_start:
+                    temp_z = temp_data
+                    z_start = False
+                else:
+                    temp_z = np.concatenate((temp_z,temp_data),axis=2)
+            
+            if y_start:
+                temp_y = temp_z
+                y_start = False
+            else:
+                temp_y = np.concatenate((temp_y,temp_z),axis=1)
+       
+        if x_start:
+            temp_x = temp_y
+            x_start = False
+        else:
+            temp_x = np.concatenate((temp_x,temp_y),axis=0)
+    
+    return temp_x
+    
+def carve_out_matrix(matrix):
+    old_shape_x, old_shape_y, old_shape_z = matrix.shape
+    x = int(round(old_shape_x / 3. ,0))
+    y = int(round(old_shape_y / 3. ,0))
+    z = int(round(old_shape_z / 3. ,0))
+   
+    return matrix[x:2*x,y:2*y,z:2*z]
+
+def get_homogeneous_gas_integral(n,r):
+    return r*r*n*math.pi
+
+def get_homo_nondimensional(int_arr, n_arr, r):
+    temp = (4./3.)*r*r*r*math.pi
+    result = np.divide(int_arr, n_arr)
+    return np.divide(result, temp)
+
+def get_homo_nondimensional_nave(int_arr, n_ave, r):
+    temp = (4./3.)*r*r*r*math.pi*n_ave
+    return np.divide(int_arr, temp)
+
+
+def calculate_ave_density_desc(n,r,hx,hy,hz,stencil,pad):
+    print 'start ave dens desc : ' + str(r) 
+#    integration, temp_pad  = get_integration_fftconv(n, hx, hy, hz, r, accuracy = get_auto_accuracy(hx,hy,hz, r))
+    integration, temp_pad = get_fftconv_with_known_stencil_no_wrap(n,hx,hy,hz,r,stencil,pad)
+    ave_density = get_homo_nondimensional_nave(integration, 1.0, r)
+    return ave_density, temp_pad
+
+def create_dataset(database, dataset_name, data):
+    if dataset_name not in database.keys():
+        database.create_dataset(dataset_name,data=data)
+    return
+
+
+def process_normal_descriptors(molecule, functional,i,j,k):
+    
+    raw_data_filename = "{}_{}_{}_{}_{}.hdf5".format(molecule,functional,i,j,k)
+    result_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
+    raw_data =  h5py.File(raw_data_filename,'r')
+    rho = np.asarray(raw_data['rho'])
+    #epxc = np.asarray(raw_data['epsilon_xc'])
+    raw_data.close()
+
+    with h5py.File(result_filename,'a') as database:
+        print 'get normal'
+        create_dataset(database, 'rho', rho)
+    #    create_dataset(database, 'epsilon_xc', epxc)
+        
+    return
+
+def process_range_descriptor(molecule, functional,i,j,k,h,N,r_list,stencil_list,pad_list,MC_surface_harmonic_stencil_dict):
+    
+    result_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
+    Nx = Ny = Nz = N
+    extented_n = read_system(molecule,functional,i,j,k,Nx,Ny,Nz,'rho')
+
+    with h5py.File(result_filename,'a') as data:
+        
+        ave_dens_grp = data.create_group('average_density')
+
+        try:
+            ave_dens_grp = data['average_density']
+        except:
+            ave_dens_grp = data.create_group('average_density')
+
+        try:
+            MC_surface_spherical_harmonic_grp = data['MC_surface_spherical_harmonic']
+        except:
+            MC_surface_spherical_harmonic_grp = data.create_group('MC_surface_spherical_harmonic')
+
+ 
+        for index, r in enumerate(r_list):
+            dataset_name = 'average_density_{}'.format(str(r).replace('.','-'))
+            if dataset_name not in ave_dens_grp.keys():
+                temp_data, temp_pad = calculate_ave_density_desc(extented_n.copy(),r,h,h,h,stencil_list[index],pad_list[index])
+                ave_dens_grp.create_dataset(dataset_name,data=carve_out_matrix(temp_data))
+
+
+        for r in r_list:
+            dataset_name = 'MC_surface_shperical_harmonic_1_{}'.format(str(r).replace('.','-'))
+            if dataset_name not in MC_surface_spherical_harmonic_grp.keys():
+                print "start: {} MC_surface_shperical 1 ".format(r)
+                stencils = MC_surface_harmonic_stencil_dict["1"][str(r)][0]
+                pad = MC_surface_harmonic_stencil_dict["1"][str(r)][1]
+
+                temp_result = np.zeros_like(carve_out_matrix(extented_n.copy()))
+
+                for temp_stencil in stencils:
+                    temp_temp_result_extend,_ = get_fftconv_with_known_stencil_no_wrap(extented_n,h,h,h,1,temp_stencil,0)
+                    temp_temp_result = carve_out_matrix(temp_temp_result_extend)
+                    temp_result = np.add(temp_result, np.square(temp_temp_result))
+
+                temp_result = np.sqrt(temp_result)
+
+                MC_surface_spherical_harmonic_grp.create_dataset(dataset_name,data=temp_result)
+                
+
+        
+    return
+
+
+def prepare_integral_stencils(r_list,h):
+    print 'start preparing integral stencils'
+    stencil_list = []
+    pad_list = []
+    for r in r_list:
+        temp_stencil,temp_pad = get_integration_stencil(h, h, h, r, accuracy = get_auto_accuracy(h,h,h, r))
+        stencil_list.append(temp_stencil)
+        pad_list.append(temp_pad)
+    return stencil_list, pad_list
+
+
+def prepare_MC_surface_harmonic_stencil_stencils(r_list,h):
+
+    MC_surface_harmonic_stencil_dict = {}
+    MC_surface_harmonic_stencil_dict["1"] = {}
+    MC_surface_harmonic_stencil_dict["2"] = {}
+    MC_surface_harmonic_stencil_dict["3"] = {}
+    MC_surface_harmonic_stencil_dict["4"] = {}
+
+    for r in r_list:
+
+        stencil_Re_1, pad =  calc_MC_surface_harmonic_stencil(h, h, h, r, 1, 1, accuracy = 6)
+        stencil_Re_2, pad =  calc_MC_surface_harmonic_stencil(h, h, h, r, 1, 2, accuracy = 6)
+        stencil_Re_3, pad =  calc_MC_surface_harmonic_stencil(h, h, h, r, 1, 3, accuracy = 6)
+        MC_surface_harmonic_stencil_dict["1"][str(r)] = [[stencil_Re_1,stencil_Re_2,stencil_Re_3], pad ]
+
+    return MC_surface_harmonic_stencil_dict
+
+
+def process(molecule, functional,i,j,k,h,N,r_list,stencil_list,pad_list,MC_surface_harmonic_stencil_dict):
+    result_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
+    if os.path.isfile(result_filename) == False:
+        print 'start {} {} {}'.format(i,j,k)
+        process_normal_descriptors(molecule, functional,i,j,k)
+        process_range_descriptor(molecule, functional,i,j,k,h,N,r_list,stencil_list,pad_list,MC_surface_harmonic_stencil_dict)
+    
+
+def process_one_molecule(molecule, functional,h,L,N,r_list):
+    cwd = os.getcwd()
+    dir_name = "{}_{}_{}_{}_{}_pure_density".format(molecule,functional,str(L).replace('.','-'),str(h).replace('.','-'),N)
+    print dir_name
+    
+    if os.path.isdir(dir_name) == False:
+        print '\n****Error: Cant find the directory! ****\n'
+        raise NotImplementedError
+    
+    os.chdir(cwd + '/' + dir_name)
+    stencil_list,pad_list = prepare_integral_stencils(r_list,h)
+    MC_surface_harmonic_stencil_dict = prepare_MC_surface_harmonic_stencil_stencils(r_list,h)
+
+    
+    Nx = Ny = Nz = N
+    i_li = range(Nx)
+    j_li = range(Ny)
+    k_li = range(Nz)
+    
+    paramlist = list(itertools.product(i_li,j_li,k_li))
+    
+
+    print "start convolution"
+    start = time.time()
+    for i,j,k in paramlist:
+        process(molecule, functional,i,j,k,h,N,r_list,stencil_list,pad_list,MC_surface_harmonic_stencil_dict)
+        print "After step {} {} {}: {}".format(i,j,k,time.time()-start)
+    print "Total time: {}".format(time.time()-start)
+    
+    
+    os.chdir(cwd)
+    return
+
+
+if __name__ == "__main__":
+
+    molecule = sys.argv[1]
+    
+    print "start"
+    h = float(sys.argv[2])
+    L = float(sys.argv[3])
+    N = int(sys.argv[4])
+    functional = "B3LYP"
+    r_list = [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2]
+
+    #for functional in functionals:
+    print "start process molecule"
+    process_one_molecule(molecule, functional,h,L,N,r_list)
---
+++ model_test/generate_data_pure_density.py
@@ -0,0 +1,313 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 19:34:19 2017
+
+@author: ray
+"""
+
+
+import os
+import h5py
+import psi4
+import sys
+import json
+import collections
+import numpy as np
+import math
+import multiprocessing
+import json
+import time
+
+def log(log_filename, text):
+    with open(log_filename, "a") as myfile:
+        myfile.write(text)
+    return
+
+# Define helper functions to re-map into 3D array
+def mapping_array(x,y,z,out_shape,x_start,x_end,y_start,y_end,z_start,z_end):
+    map_array = np.zeros(out_shape,dtype='int')
+    npoints_x, npoints_y, npoints_z = out_shape
+ 
+    def get_index(xi,x_start,x_end,npoints):
+        xL = list(np.linspace(x_start, x_end, npoints))
+        idx = xL.index(xi)
+        return idx
+    for i,xi,yi,zi in zip(range(len(x)),x,y,z):
+        xid = get_index(xi,x_start,x_end,npoints_x)
+        yid = get_index(yi,y_start,y_end,npoints_y)
+        zid = get_index(zi,z_start,z_end,npoints_z)
+        map_array[xid,yid,zid] = int(i)
+    return map_array.ravel()
+
+def remap(in_vector, map_array, out_shape):
+    in_vector = in_vector[map_array].reshape(out_shape)
+    return in_vector
+    
+
+def process_one_section(x,y,z,w,x_start,x_end,y_start,y_end,z_start,z_end,out_shape,scf_wfn,scf_e):
+        
+    # Get DFT density
+    C = np.array(scf_wfn.Ca_subset("AO", "OCC"))
+    D = np.dot(C, C.T)
+    DFT_Density = psi4.core.Matrix.from_array(D)
+    
+    # Set up Vxc functional
+    Vpot = scf_wfn.V_potential()
+    superfunc = Vpot.functional()
+    
+    
+    # In terms of Psi4 vector objects
+    xvec = psi4.core.Vector.from_array(x)
+    yvec = psi4.core.Vector.from_array(y)
+    zvec = psi4.core.Vector.from_array(z)
+    wvec = psi4.core.Vector.from_array(w)
+
+    # Compute the spatial extent computed
+    # Used for sieving
+    extents = psi4.core.BasisExtents(scf_wfn.basisset(), 1.e-32)
+    # Build a "block-O-points"
+    grid = psi4.core.BlockOPoints(xvec, yvec, zvec, wvec, extents)
+
+    # Need a proper points funciton
+    points_func = psi4.core.RKSFunctions(scf_wfn.basisset(), x.shape[0], scf_wfn.basisset().nbf())
+    points_func.set_deriv(2)
+    points_func.set_ansatz(2)
+    points_func.set_pointers(DFT_Density)
+
+    # Make sure the SuperFunctional has room
+    superfunc.set_max_points(x.shape[0])
+    superfunc.allocate()
+
+    # Drop the outputs here
+    ret = collections.defaultdict(list)
+    
+    # Grab a grid "block"
+    w = np.array(grid.w())
+    x = np.array(grid.x())
+    y = np.array(grid.y())
+    z = np.array(grid.z())
+
+    # Global to local map
+    gmap = np.array(grid.functions_local_to_global())
+
+    # What size do I need to slice out of my buffers?
+    npoints = w.shape[0]
+    nfunc = gmap.shape[0]
+
+    # Copmute the given grid
+    points_func.compute_points(grid)
+
+    # Grab quantities on a grid
+    rho = np.array(points_func.point_values()["RHO_A"])[:npoints]
+#    rho_x = np.array(points_func.point_values()["RHO_AX"])[:npoints]
+#    rho_y = np.array(points_func.point_values()["RHO_AY"])[:npoints]
+#    rho_z = np.array(points_func.point_values()["RHO_AZ"])[:npoints]
+#    gamma = np.array(points_func.point_values()["GAMMA_AA"])[:npoints]
+#    tau = np.array(points_func.point_values()["TAU_A"])[:npoints]
+#    gradient = rho_x + rho_y + rho_z
+
+    # Compute our functional
+#    dft_results = superfunc.compute_functional(points_func.point_values(), -1)
+
+    # Append outputs
+#    ret["w"].append(w)
+#    ret["x"].append(x)
+#    ret["y"].append(y)
+#    ret["z"].append(z)
+
+    ret["rho"].append(rho)
+#    ret["gradient"].append(gradient)
+#    ret["gamma"].append(gamma)
+#    ret["tau"].append(tau)
+
+#    ret["epsilon_xc"].append(np.array(dft_results["V"])[:npoints])
+#    ret["V_xc"].append(np.array(dft_results["V_RHO_A"])[:npoints])
+    
+    # Reformat outputs into 3D array format
+    map_array = mapping_array(x,y,z,out_shape,x_start,x_end,y_start,y_end,z_start,z_end)
+    output = {k : remap(np.hstack(v),map_array,out_shape) for k, v in ret.items()}
+    
+
+
+    return output
+    
+def process(X0,Y0,Z0,x_inc,y_inc,z_inc,hx,hy,hz,i,j,k ,dv,scf_wfn,scf_e):
+
+    x_start = X0 + float(i) * x_inc
+    y_start = Y0 + float(j) * y_inc
+    z_start = Z0 + float(k) * z_inc
+    
+    x_end = x_start + x_inc - hx
+    y_end = y_start + y_inc - hy
+    z_end = z_start + z_inc - hz
+    
+    print "\n x: {}:{} \t {}:{} \t {}:{}".format(x_start,x_end,y_start,y_end,z_start,z_end)
+    
+    xyz = []
+    for hi, start,end in zip((hx, hy, hz),(x_start, y_start ,z_start),(x_end, y_end ,z_end)):
+        n_i = ((end-start)/hi)+1.
+        xi = np.linspace(start, end, int(round(n_i,0)))
+        xyz.append(xi)
+
+    out_shape = [len(xi) for xi in xyz]
+    x, y, z = np.meshgrid(*xyz)
+    x = x.ravel()
+    y = y.ravel()
+    z = z.ravel()
+    w = np.ones_like(z)*dv
+    
+    temp_filename =  '{}_{}_{}_{}_{}.hdf5'.format(molecule_name,xc,i,j,k)
+    #if os.path.isfile(temp_filename) == False:
+    temp_out = process_one_section(x,y,z,w,x_start,x_end,y_start,y_end,z_start,z_end,out_shape,scf_wfn,scf_e)
+#        print type(temp_out['epsilon_xc'])
+#        print temp_out['epsilon_xc'].shape
+    
+    with h5py.File(temp_filename) as data:
+        for key in temp_out:
+            data.create_dataset(key,data=temp_out[key])
+        data.create_dataset('h_x',data=[h])
+        data.create_dataset('h_y',data=[h])
+        data.create_dataset('h_z',data=[h])
+    #block_fd_Exc = np.sum(temp_out["epsilon_xc"])
+    #print "Total Exc: {}".format(block_fd_Exc)
+
+
+    #return block_fd_Exc
+    return
+    
+def process_system(molecule, molecule_name, xc, h, cell, num_blocks, psi4_options=None):
+
+    log_filename = "{}_log.log".format(molecule_name)
+
+    cwd = os.getcwd()
+    dir_name = "{}_{}_{}_{}_{}_pure_density".format(molecule_name,xc,str(cell).replace('.','-'),str(h).replace('.','-'),num_blocks)
+    
+    if os.path.isdir(dir_name) == False:
+        os.makedirs(cwd + '/' + dir_name)
+    
+    os.chdir(cwd + '/' + dir_name)
+    
+    if psi4_options == None:
+        psi4_options = {"BASIS": "aug-cc-pvtz",
+                    "D_CONVERGENCE":1e-9,
+                    "E_CONVERGENCE":1e-9,
+                  'DFT_BLOCK_MAX_POINTS': 500000,
+                  'DFT_BLOCK_MIN_POINTS': 100000,
+                  'MAXITER': 500,
+#                  'DFT_SPHERICAL_POINTS': 302,
+#                  'DFT_RADIAL_POINTS':    75,
+                  "SAVE_JK": True, }
+    psi4.set_options(psi4_options)
+    
+    if isinstance(h,float):
+        hx = hy = hz = h
+    elif len(h) == 3:
+        hx, hy, hz = h
+    else:
+        raise Exception('Invalid grid spacing')
+
+    dv = hx*hy*hz #volume element size (used for integration)
+    
+    if isinstance(cell,float) or isinstance(cell,int): 
+        Lx = Ly = Lz = float(cell)
+    elif len(cell) == 3:
+        Lx, Ly, Lz = cell
+    else:
+        raise Exception('Invalid cell')
+    
+    if  isinstance(num_blocks,int): 
+        Nx = Ny = Nz = int(num_blocks)
+    elif len(num_blocks) == 3:
+        Nx, Ny, Nz = num_blocks
+    else:
+        raise Exception('Invalid block dividing')
+    
+    time_start = time.time()
+
+    scf_e, scf_wfn = psi4.energy(xc, molecule=molecule, return_wfn=True)
+
+    time_scf = time.time() - time_start
+
+    print "SCF time: {}".format(time_scf)
+    
+    X0 = -Lx/2.
+    Y0 = -Ly/2.
+    Z0 = -Lz/2.
+
+    x_inc = Lx/Nx
+    y_inc = Ly/Ny
+    z_inc = Lz/Nz
+
+    #Total_fd_Exc = 0.0
+    for i in range(Nx):
+        for j in range(Ny):
+            for k in range(Nz):
+                process(X0,Y0,Z0,x_inc,y_inc,z_inc,hx,hy,hz,i,j,k ,dv,scf_wfn,scf_e)
+    #            Total_fd_Exc += temp_block_fd_Exc
+
+    time_total = time.time() - time_start
+
+    
+    os.chdir(cwd)
+    #log(log_filename, "SCF time: {} \nTotal time: {}".format(time_scf,time_total))
+    print "SCF time: {} \nTotal time: {}".format(time_scf,time_total)
+    #print "Total fd Exc: {}".format(Total_fd_Exc)
+    return
+
+
+
+
+
+
+
+
+def read_json_data(data):
+    result = ''
+    for i in range(len(data['atoms'])):
+        temp = '{}\t{}\t{}\t{}\n'.format(data['atoms'][i], data['coordinates'][i][0],data['coordinates'][i][1],data['coordinates'][i][2])
+        result += temp
+    result += '\t symmetry {}'.format(data['symmetry'])
+    return psi4.geometry(result)
+
+
+
+
+if __name__ == "__main__":
+
+    database_filename = sys.argv[1]
+    molecule_name = sys.argv[2]
+    h = float(sys.argv[3])
+    L = float(sys.argv[4])
+    N = int(sys.argv[5])
+    
+
+    molecule_names = [molecule_name]
+    
+    try:
+        data = json.load(open(database_filename,'rb'))
+    except:
+        with open(database_filename, encoding='utf-8') as f:
+            data=json.load(f)
+    
+    molecules = {}
+    for molecule in molecule_names:
+        if molecule in data:
+            molecules[molecule] = read_json_data(data[molecule])
+            
+
+    xc = "B3LYP"
+    all_data = {}
+    
+    
+        
+    failed_filename = "failed_molecule.log"
+    succ_filename = "successful_molecule.log"
+    cwd = os.getcwd()
+
+    for mol in molecules:
+        filename = '{}_{}.hdf5'.format(mol,xc)
+        if not os.path.exists(filename):
+
+            process_system(molecules[mol],mol,xc,h,L,N)
+
+
