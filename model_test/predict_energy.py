0fa4f5cdd427ac41364e6924c8fbd76e9c8c6356
ray38 <xlei38@gatech.edu>
2018-09-21 13:50:30 -0400

    test

--- model_test/predict_energy.py
+++ model_test/predict_energy.py
@@ -153,13 +153,11 @@ def transform_data(temp_data, transform):
     elif transform == "neglog10":
         return np.log10(np.multiply(-1., temp_data.flatten())).tolist()
     
-def load_data_each_block(molecule,functional,i,j,k, dataset_setup, data_dir_full):
+def load_data_each_block(molecule,functional,i,j,k, dataset_setup, molecule_data_directory):
     data_filename = "{}_{}_{}_{}_{}_all_descriptors.hdf5".format(molecule,functional,i,j,k)
 
-    os.chdir(data_dir_full)
+    os.chdir(molecule_data_directory)
 
-    print data_dir_full
-    print os.getcwd()
 
     data =  h5py.File(data_filename,'r')
     
@@ -169,7 +167,6 @@ def load_data_each_block(molecule,functional,i,j,k, dataset_setup, data_dir_full
     dens = []
     
     target = dataset_setup['target']
-    
  
     if target == 'epxc':
         target_set_name = 'epsilon_xc'
@@ -185,25 +182,70 @@ def load_data_each_block(molecule,functional,i,j,k, dataset_setup, data_dir_full
         result_list.append(transform_data(temp_data, dataset_setup['density_transform']))
         dens.append(transform_data(temp_data, dataset_setup['density_transform']))
 
+    if int(dataset_setup['gamma']) == 1:
+        temp_data = np.asarray(data['gamma'])
+        result_list.append(transform_data(temp_data, dataset_setup['gamma_transform']))
+
+    if int(dataset_setup['tau']) == 1:
+        temp_data = np.asarray(data['tau'])
+        result_list.append(transform_data(temp_data, dataset_setup['tau_transform']))
 
 
+    group_name = 'derivative'
+    temp_list = dataset_setup["derivative_list"]
+    if len(temp_list) > 0: 
+        for derivative_count in temp_list:
+            dataset_name = 'derivative_{}'.format(derivative_count)
+            temp_data = np.asarray(data[group_name][dataset_name])
+            result_list.append(transform_data(temp_data, dataset_setup['derivative_transform']))
+
+    temp_list = dataset_setup["derivative_square_list"]
+    if len(temp_list) > 0: 
+        for derivative_count in temp_list:
+            dataset_name = 'derivative_{}'.format(derivative_count)
+            temp_data = np.power(np.asarray(data[group_name][dataset_name]), 2.)
+            result_list.append(transform_data(temp_data, dataset_setup['derivative_square_transform']))
     
    
     group_name = 'average_density'
-    try:
-        temp_list = dataset_setup["average_density_r_list"]
-        if len(temp_list) > 0:
-            for r_list_count in temp_list:
-                dataset_name = 'average_density_{}'.format(str(r_list_count).replace('.','-'))
-                temp_data = np.asarray(data[group_name][dataset_name])
-                result_list.append(transform_data(temp_data, dataset_setup['average_density_transform']))
+    temp_list = dataset_setup["average_density_r_list"]
+    if len(temp_list) > 0:
+        for r_list_count in temp_list:
+            dataset_name = 'average_density_{}'.format(str(r_list_count).replace('.','-'))
+            temp_data = np.asarray(data[group_name][dataset_name])
+            result_list.append(transform_data(temp_data, dataset_setup['average_density_transform']))
 
-    except:
-        pass
 
+    group_name = 'asym_integral'
+    temp_list = dataset_setup["asym_desc_r_list"]
+    if len(temp_list) > 0:
+        for r_list_count in temp_list:
+            dataset_name = 'asym_integral_x_{}'.format(str(r_list_count).replace('.','-'))
+            temp_data = np.asarray(data[group_name][dataset_name])
+            result_list.append(transform_data(temp_data, dataset_setup['asym_desc_transform']))
+
+            dataset_name = 'asym_integral_y_{}'.format(str(r_list_count).replace('.','-'))
+            temp_data = np.asarray(data[group_name][dataset_name])
+            result_list.append(transform_data(temp_data, dataset_setup['asym_desc_transform']))
+
+            dataset_name = 'asym_integral_z_{}'.format(str(r_list_count).replace('.','-'))
+            temp_data = np.asarray(data[group_name][dataset_name])
+            result_list.append(transform_data(temp_data, dataset_setup['asym_desc_transform']))
 
     
     group_name = 'MC_surface_spherical_harmonic'
+
+    try:
+        temp_list = dataset_setup["MC_surface_spherical_harmonic_0_r_list"]
+        if len(temp_list) > 0:
+            for r_list_count in temp_list:
+                dataset_name = 'MC_surface_shperical_harmonic_0_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, dataset_setup['MC_surface_spherical_harmonic_0_transform']))
+    except:
+        pass
+
+    
     try:
         temp_list = dataset_setup["MC_surface_spherical_harmonic_1_r_list"]
         if len(temp_list) > 0:
@@ -214,29 +256,52 @@ def load_data_each_block(molecule,functional,i,j,k, dataset_setup, data_dir_full
     except:
         pass
 
+    try:
+        temp_list = dataset_setup["MC_surface_spherical_harmonic_2_r_list"]
+        if len(temp_list) > 0:
+            for r_list_count in temp_list:
+                dataset_name = 'MC_surface_shperical_harmonic_2_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, dataset_setup['MC_surface_spherical_harmonic_2_transform']))
+    except:
+        pass
+
+    try:
+        temp_list = dataset_setup["MC_surface_spherical_harmonic_3_r_list"]
+        if len(temp_list) > 0:
+            for r_list_count in temp_list:
+                dataset_name = 'MC_surface_shperical_harmonic_3_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, dataset_setup['MC_surface_spherical_harmonic_3_transform']))
+    except:
+        pass
+
+    try:
+        temp_list = dataset_setup["MC_surface_spherical_harmonic_4_r_list"]
+        if len(temp_list) > 0:
+            for r_list_count in temp_list:
+                dataset_name = 'MC_surface_shperical_harmonic_4_{}'.format(str(r_list_count).replace('.','-'))
+                temp_data = np.asarray(data[group_name][dataset_name])
+                result_list.append(transform_data(temp_data, dataset_setup['MC_surface_spherical_harmonic_4_transform']))
+    except:
+        pass
 
 
 
     result = zip(*result_list)
     y = zip(*y)
     dens = zip(*dens)
-    print "done loading: {} {} {}".format(i,j,k)
 
-    os.chdir(setup["model_save_dir"])
+    os.chdir(setup["working_dir"])
     return np.asarray(dens), np.asarray(result), np.asarray(y)
 
+def process_each_block(molecule, i,j,k, setup, molecule_data_directory):
 
-def process_each_block(molecule, i,j,k, setup, data_dir_full):
-    print 'started'
-
-    h = float(setup['grid_spacing'])
-    L = float(setup['box_dimension'])
-    N = int(setup['number_segment_per_side'])
+    h = setup['grid_spacing']
     functional = setup['functional']
-    #log_filename = setup["predict_full_log_name"]
 
     start = time.time()
-    dens, X,y = load_data_each_block(molecule,functional,i,j,k, setup["dataset_setup"], data_dir_full)
+    dens, X,y = load_data_each_block(molecule,functional,i,j,k, setup["dataset_setup"], molecule_data_directory)
     original_y, predict_y = predict_each_block(setup, dens, X, y)
     dv = h*h*h
     y = original_y * dv*27.2114
@@ -244,52 +309,40 @@ def process_each_block(molecule, i,j,k, setup, data_dir_full):
     y_sum = np.sum(y)
     y_predict_sum = np.sum(y_predict)
     error = y-y_predict
-    
-#    fraction_error = np.divide(error, y)
+    absolute_error = np.abs(error)
+
     sum_error = y_sum - y_predict_sum
+    sum_absolute_error = np.sum(absolute_error)
     
-    os.chdir(setup["model_save_dir"])
+    os.chdir(setup["working_dir"])
     log(setup["predict_full_log_name"],"\ndone predicting: " + molecule + "\t took time: " + str(time.time()-start)+ "\t" + str(i) + "\t" + str(j) + "\t" + str(k))
-    log(setup["predict_full_log_name"],"\nenergy: " + str(y_sum) + "\tpredicted energy: " + str(y_predict_sum) + "\tpredicted error: " + str(sum_error)) 
-
-    os.chdir(setup["model_save_dir"])
-
-    return sum_error, y_predict_sum, y_sum   
+    log(setup["predict_full_log_name"],"\nenergy: " + str(y_sum) + "\tpredicted energy: " + str(y_predict_sum) + "\tpredict error: " + str(sum_error) + "\tabsolute error: " + str(sum_absolute_error))
 
 
+    return sum_error, sum_absolute_error,y_predict_sum, y_sum 
 
-def process_one_molecule(molecule, setup):
+
+
+def process_one_molecule(molecule, molecule_data_directory, setup):
  
 
-    h = float(setup['grid_spacing'])
-    L = float(setup['box_dimension'])
-    N = int(setup['number_segment_per_side'])
-
-    functional = setup['functional']  
-
-#    log_filename = setup["predict_log_name"]
-    
-    data_dir_name = "{}_{}_{}_{}_{}".format(molecule,functional,str(L).replace('.','-'),str(h).replace('.','-'),N)
-
-
-    data_dir_full = setup["sub_database"]  + "/" + data_dir_name
-
-#    os.chdir(data_dir_full)
-
+    N = setup["N"]
     Nx = Ny = Nz = N
     i_li = range(Nx)
     j_li = range(Ny)
     k_li = range(Nz)
     
     system_sum_error = 0 
+    system_sum_absolute_error = 0
     system_y_predict_sum = 0
     system_y_sum = 0 
     
     paramlist = list(itertools.product(i_li,j_li,k_li))
     for i,j,k in paramlist:
         
-        sum_error, y_predict_sum, y_sum = process_each_block(molecule, i,j,k, setup, data_dir_full)
+        sum_error, sum_absolute_error,y_predict_sum, y_sum = process_each_block(molecule, i,j,k, setup, molecule_data_directory)
         system_sum_error += sum_error
+        system_sum_absolute_error += sum_absolute_error
         system_y_predict_sum += y_predict_sum
         system_y_sum += y_sum
 
@@ -301,171 +354,69 @@ def process_one_molecule(molecule, setup):
     log(setup["predict_full_log_name"],"\n\ndone predicting: " + molecule )
     log(setup["predict_full_log_name"],"\nenergy: " + str(system_y_sum) + "\tpredicted energy: " + str(system_y_predict_sum) + "\tpredicted error: " + str(system_sum_error)+ "\n") 
     
-    log(setup["predict_error_log_name"], "\n{}\t{}\t{}\t{}".format(molecule, system_y_sum, system_y_predict_sum, system_sum_error))
-    return system_sum_error, system_y_predict_sum,system_y_sum
+    log(setup["predict_error_log_name"], "\n{}\t{}\t{}\t{}\t{}".format(molecule, system_y_sum, system_y_predict_sum, system_sum_error,system_sum_absolute_error))
+    return system_sum_error,system_sum_absolute_error, system_y_predict_sum,system_y_sum
 
     
 
 
-def initialize(setup,NN_model_filename):
-    os.chdir(setup["model_save_dir"])
-    fit_log_name = "NN_fit_log.log"
-    
-    LDA_model_name = "LDA_model.sav"
-    NN_model_name = NN_model_filename
-    #NN_model_name = "NN.h5"
-
-    if NN_model_name == "NN.h5":
-
-        loss_value,loss = get_start_loss(fit_log_name)
-        predict_log_name = "predict_{}_{}_log.log".format(loss,loss_value)
-        predict_full_log_name = "predict_{}_{}_full_log.log".format(loss,loss_value)
-        predict_error_log_name = "predict_{}_{}_error_log.log".format(loss,loss_value)
-        predict_formation_log_name = "predict_{}_{}_formation_log.log".format(loss,loss_value)
-        try:
-            NN_model = load_model(NN_model_name, custom_objects={'sae': sae})
-        except:
-            NN_model = load_model(NN_model_name)
-        NN_model.save("NN_{}_{}_backup.h5".format(loss,loss_value))
+def initialize(setup,NN_model_filename,LDA_model_filename):
 
 
-    else:
-        temp = NN_model_name.split("_")
-        if temp[1] == "mean" and temp[2] == "absolute" and temp[3] == "percentage":
-            loss = "mean_absolute_percentage_error"
-            loss_value = temp[5]
-        else:
-            loss = temp[1]
-            loss_value = temp[2]
-        predict_log_name = "predict_{}_{}_log.log".format(loss,loss_value)
-        predict_full_log_name = "predict_{}_{}_full_log.log".format(loss,loss_value)
-        predict_error_log_name = "predict_{}_{}_error_log.log".format(loss,loss_value)
-        predict_formation_log_name = "predict_{}_{}_formation_log.log".format(loss,loss_value)
-        try:
-            NN_model = load_model(NN_model_name, custom_objects={'sae': sae})
-        except:
-            NN_model = load_model(NN_model_name)
+    try:
+        NN_model = load_model(NN_model_filename, custom_objects={'sae': sae})
+    except:
+        NN_model = load_model(NN_model_filename)
 
+    LDA_model = pickle.load(open(LDA_model_filename, 'rb'))
 
+    predict_log_name = "predict_log.log"
+    predict_full_log_name = "predict_full_log.log"
+    predict_error_log_name = "predict_error_log.log"
 
-    LDA_model = pickle.load(open(LDA_model_name, 'rb'))
 
     setup["NN_model"] = NN_model
     setup["LDA_model"] = LDA_model
     setup["predict_log_name"] = predict_log_name
     setup["predict_full_log_name"] = predict_full_log_name
     setup["predict_error_log_name"] = predict_error_log_name
-    setup["predict_formation_log_name"] = predict_formation_log_name
 
-
-
-    os.chdir(setup["working_dir"])
     return
 
 
 if __name__ == "__main__":
 
-
-
-
-    print "start"
-    predict_setup_filename = sys.argv[1]
-    dataset_setup_database_filename = sys.argv[2]
-    dataset_name = sys.argv[3]
-    NN_model_filename = sys.argv[4]
+    dataset_setup_database_filename = sys.argv[1]
+    dataset_name = sys.argv[2]
+    NN_model_filename = sys.argv[3]
+    LDA_model_filename = sys.argv[4]
+    molecule = sys.argv[5]
+    molecule_data_directory = sys.argv[6]
+    num_box_per_side = int(sys.argv[7])
 
     K.set_floatx('float64')
     K.floatx()
 
+    setup = {}
+    setup["NN_model_filename"] = NN_model_filename
+    setup["LDA_model_filename"] = LDA_model_filename
+    setup["N"] = num_box_per_side
+    setup["functional"] = "B3LYP"
+    setup['grid_spacing'] = 0.02
 
-    with open(predict_setup_filename) as f:
-        setup = json.load(f)
-
-
-    h = float(setup['grid_spacing'])
-    L = float(setup['box_dimension'])
-    N = int(setup['number_segment_per_side'])
-
-    setup['functional'] = functional
-#    functional = setup['functional']
 
     with open(dataset_setup_database_filename) as f:
         setup_database = json.load(f)
-
     dataset_setup = setup_database[dataset_name]
 
     setup["dataset_setup"] = dataset_setup
-
-    dir_name = "{}_{}_{}".format(str(L).replace('.','-'),str(h).replace('.','-'),N)
-
-    working_dir = os.getcwd() + '/' + dir_name + '/' + dataset_name
-    setup["working_dir"] = working_dir
-
-    model_save_dir = working_dir + "/" + "NN_{}_{}_{}_{}".format(setup["fit_type"],setup["NN_setup"]["number_neuron_per_layer"], setup["NN_setup"]["number_layers"], setup["NN_setup"]["activation"])
-    setup["model_save_dir"] = model_save_dir
+    setup["working_dir"] = os.getcwd()
 
 
-    database_name = setup["database_directory"]
-    sub_database_name = "{}_{}_{}".format(str(L).replace('.','-'),str(h).replace('.','-'),N)
-
-    setup["sub_database"] = database_name + '/' + sub_database_name
 
     initialize(setup,NN_model_filename)
 
-    setup["result_data"] = {}
-
-    error_list = []
-
-    for molecule in setup["molecule_list"]:
-        setup["result_data"][molecule] = {}
-        setup["result_data"][molecule]["exist"] = False
-
-    
-
-    os.chdir(setup["model_save_dir"])
-
-    try:
-        with open(setup["predict_error_log_name"],'rb') as f:
-            for line in f:
-                if line.strip() != '':
-                    temp = line.strip().split()
-                    temp_name = temp[0]
-                    temp_original_energy = float(temp[1])
-                    temp_predict_energy  = float(temp[2])
-                    temp_error  = float(temp[3])
-                    setup["result_data"][temp_name]['predict_exc'] = temp_predict_energy
-                    setup["result_data"][temp_name]['original_exc'] = temp_original_energy
-                    setup["result_data"][temp_name]["exist"] = True
-                    error_list.append(temp_error)
-
-    except:
-        pass
-
-
-
-    print setup["result_data"]
-
-
-    
-    for molecule in setup["molecule_list"]:
-        if setup["result_data"][molecule]["exist"] == False:
-            #try:
-                
-            temp_error,temp_y_predict,temp_y = process_one_molecule(molecule, setup)
-            error_list.append(temp_error)
-
-            #setup["result_data"][molecule] = {}
-            setup["result_data"][molecule]['predict_exc'] = temp_y_predict
-            setup["result_data"][molecule]['original_exc'] = temp_y
-    
-
-    log(setup["predict_log_name"],"\n\naverage error: " + str(np.mean(error_list)) + "\tstddev error: " + str(np.std(error_list))) 
-    log(setup["predict_log_name"],"\n\naverage abs error: " + str(np.mean(np.abs(error_list))) + "\tstddev abs error: " + str(np.std(np.abs(error_list))))
-    log(setup["predict_log_name"],"\n\naverage abs error: " + str(sqrt(mean(square(error_list))))) 
-
-    log(setup["predict_full_log_name"],"\n\naverage error: " + str(np.mean(error_list)) + "\tstddev error: " + str(np.std(error_list))) 
-    log(setup["predict_full_log_name"],"\n\naverage abs error: " + str(np.mean(np.abs(error_list))) + "\tstddev abs error: " + str(np.std(np.abs(error_list))))
-    log(setup["predict_full_log_name"],"\n\naverage abs error: " + str(sqrt(mean(square(error_list))))) 
+    temp_error,temp_absolute_error, temp_y_predict,temp_y = process_one_molecule(molecule,molecule_data_directory, setup)
 
 
 
