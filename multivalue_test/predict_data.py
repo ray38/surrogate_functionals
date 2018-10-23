4d9adfc2f3ca855f22bda993f778412e515b25f4
ray38 <xlei38@gatech.edu>
2018-10-23 16:26:32 -0400

    test

---
+++ multivalue_test/predict_data.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 09:43:05 2017
+
+@author: ray
+"""
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.pyplot as plt
+
+import numpy as np
+import csv
+import sys
+import os
+import time
+import math
+import json
+from glob import glob
+from sklearn import linear_model
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense, Activation
+from keras import backend as K
+import keras
+import scipy
+
+import itertools
+import multiprocessing
+
+try: import cPickle as pickle
+except: import pickle
+import matplotlib.pyplot as plt
+import keras.backend as K
+
+def sae(y_true, y_pred):
+    return K.sum(K.abs(y_pred - y_true))
+
+def map_to_n1_1(arr, maxx, minn):
+    return np.divide(np.subtract(arr,minn),(maxx-minn)/2.)-1.
+    
+def log(log_filename, text):
+    with open(log_filename, "a") as myfile:
+        myfile.write(text)
+    return
+
+
+def write(log_filename, text):
+    with open(log_filename, "w") as myfile:
+        myfile.write(text)
+    return
+
+def map_to_0_1(arr, maxx, minn):
+    return np.divide(np.subtract(arr,minn),(maxx-minn))
+    
+def map_back(arr, maxx, minn):
+    return np.add(np.multiply(arr,(maxx-minn)),minn)
+
+
+
+
+
+def get_x0():
+    x = [ -0.45816529328314287, 0.031091, 0.21370, 7.5957, 3.5876, 1.6382, 0.49294]
+    return x
+
+def optimization_constants(x):
+    #C0I = x[0]
+    #C1  = x[1]
+    #CC1 = x[2]
+    #CC2 = x[3]
+    #IF2 = x[4]
+
+    C1  = x[0]
+    gamma = x[1]
+    alpha1 = x[2]
+    beta1 = x[3]
+    beta2 = x[4]
+    beta3 = x[5]
+    beta4 = x[6]
+
+    #return C0I, C1, CC1, CC2, IF2, gamma, alpha1, beta1, beta2, beta3, beta4
+    return C1, gamma, alpha1, beta1, beta2, beta3, beta4
+
+def G(rtrs, gamma, alpha1, beta1, beta2, beta3, beta4):
+    Q0 = -2.0 * gamma * (1.0 + alpha1 * rtrs * rtrs)
+    Q1 = 2.0 * gamma * rtrs * (beta1 +
+                           rtrs * (beta2 +
+                                   rtrs * (beta3 +
+                                           rtrs * beta4)))
+    G1 = Q0 * np.log(1.0 + 1.0 / Q1)
+    return G1
+
+def lda_x( n, x):
+#    C0I, C1, CC1, CC2, IF2 = lda_constants()
+    C1, gamma, alpha1, beta1, beta2, beta3, beta4 = optimization_constants(x)
+
+    C0I = 0.238732414637843
+    #C1 = -0.45816529328314287
+    rs = (C0I / n) ** (1 / 3.)
+    ex = C1 / rs
+    return n*ex
+    #e[:] += n * ex
+
+def lda_c( n, x):
+    #C0I, C1, CC1, CC2, IF2 = lda_constants()
+    C1, gamma, alpha1, beta1, beta2, beta3, beta4 = optimization_constants(x)
+
+    C0I = 0.238732414637843
+    #C1 = -0.45816529328314287
+    rs = (C0I / n) ** (1 / 3.)
+    ec = G(rs ** 0.5, gamma, alpha1, beta1, beta2, beta3, beta4)
+    return n*ec
+    #e[:] += n * ec
+
+def predict_LDA(n,LDA_x):
+
+    n = np.asarray(n)
+
+    return lda_x(n,LDA_x) + lda_c(n,LDA_x)
+
+def predict_LDA_residual(n,LDA_x,X,NN_model):
+
+    n = np.asarray(n)
+
+    return lda_x(n,LDA_x) + lda_c(n,LDA_x) + ((NN_model.predict(X*1e6))/1e6)
+
+
+
+def get_data(datafile):
+
+    overall = pickle.load(open(datafile,'rb'))
+
+
+
+    X_train = []
+    y_train = []
+    dens = []
+
+    for entry in overall:
+#        if entry[0] >= lower and entry[0] <= upper:
+        X_train.append(list(entry[1:]))
+        dens.append(entry[1])
+        y_train.append(entry[0])
+    
+    
+    X_train = (np.asarray(X_train))
+    y_train = np.asarray(y_train).reshape((len(y_train),1))
+    dens = np.asarray(dens).reshape((len(dens),1))
+    
+    return X_train, y_train, dens
+
+
+def fit_model(LDA_result, dens, X_train, residual, y, loss, tol, slowdown_factor, early_stop_trials):
+
+    NN_model,loss_result = fit_with_KerasNN(X_train * 1e6, residual * 1e6, loss, tol, slowdown_factor, early_stop_trials)
+    save_resulting_figure(dens,LDA_result.x,X_train,NN_model,y,loss,loss_result)
+
+    return NN_model
+
+
+if __name__ == "__main__":
+
+
+    LDA_model_filename = sys.argv[1]
+    model_filename = sys.argv[2]
+    datafile = sys.argv[3]
+    h = 0.02
+    dv = h*h*h
+
+
+    K.set_floatx('float64')
+    K.floatx()
+
+
+
+    LDA_model = pickle.load(open(LDA_model_filename, 'rb'))
+    model = load_model(model_filename, custom_objects={'sae': sae})
+    
+    
+    X,y, dens = get_data(datafile)
+   
+
+    residual, LDA_result = fit_with_LDA(dens,y)
+    y_predict = predict_LDA_residual(dens,LDA_model.x,X,NN_model)
+    error = y - y_predict
+
+
+    error = error *dv*27.2114
+    y = y *dv*27.2114
+    y_predict = y_predict*dv*27.2114
+
+    result = {}
+    result["y"] = y
+    result["dens"] = dens
+    result["y_predict"] = y_predict
+    result["error"] = error
+
+    
+    with open("predicted" + datafile, 'wb') as handle:
+        pickle.dump(result, handle, protocol=2)
+
+    
