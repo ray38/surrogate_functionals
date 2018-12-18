9426bae3662cadde99f763f69b24c62b05fd8ac0
ray38 <xlei38@gatech.edu>
2018-12-18 11:39:50 -0500

    test

--- psi4_fit_LDA_residual_json_float64_1M_customize_continuous_fit.py
+++ psi4_fit_LDA_residual_json_float64_1M_customize_continuous_fit.py
@@ -126,10 +126,10 @@ def fit_with_KerasNN(X, y, loss, tol, slowdown_factor, early_stop_trials):
     est_start = time.time()
     history_callback = model.fit(X, y, nb_epoch=1, batch_size=50000, shuffle=True)
     est_epoch_time = time.time()-est_start
-    if est_epoch_time >= 30.:
+    if est_epoch_time >= 20.:
         num_epoch = 1
     else:
-        num_epoch = int(math.floor(30./est_epoch_time))
+        num_epoch = int(math.floor(20./est_epoch_time))
     if restart == True:
         try:
             start_loss = get_start_loss(log_filename,loss)
