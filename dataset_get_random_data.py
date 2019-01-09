4a97faa0103b180dffc6b3331b3c45ee8e380ec8
ray38 <xlei38@gatech.edu>
2019-01-09 12:02:43 -0500

    test

---
+++ dataset_get_random_data.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 09:43:05 2017
+
+@author: ray
+"""
+
+
+import numpy as np
+import csv
+import sys
+import os
+import time
+import math
+import json
+from glob import glob
+from sklearn import linear_model
+import scipy
+
+import itertools
+import multiprocessing
+
+try: import cPickle as pickle
+except: import pickle
+import matplotlib.pyplot as plt
+from subsampling import subsampling_system,random_subsampling,subsampling_system_with_PCA
+
+
+
+def read_data_from_one_dir(directory):
+    temp_cwd = os.getcwd()
+    os.chdir(directory)
+
+    print directory
+
+    subsampled_filename = "overall_subsampled_data.p"
+    random_filename = "overall_random_data.p"
+
+    try:
+        molecule_subsampled_data = pickle.load(open(subsampled_filename,'rb'))
+        print "read subsampled data"
+    except:
+        molecule_subsampled_data = []
+
+    try:
+        molecule_random_data = pickle.load(open(random_filename,'rb'))
+        print "read random data"
+    except:
+        molecule_random_data = []
+    #molecule_random_data = []
+
+    os.chdir(temp_cwd)
+
+    return molecule_subsampled_data, molecule_random_data
+
+
+
+def get_training_data(dataset_name,setup):
+
+    data_dir_name = setup["working_dir"] + "/data/*/" 
+    data_paths = glob(data_dir_name)
+    print data_paths
+
+
+    overall_subsampled_data = []
+    overall_random_data = []
+    num_samples = len(data_paths)
+    num_random_per_molecule = int(math.ceil(float(setup["random_pick"])/float(num_samples)))
+    for directory in data_paths:
+        temp_molecule_subsampled_data, temp_molecule_random_data = read_data_from_one_dir(directory)
+        overall_subsampled_data += temp_molecule_subsampled_data
+        overall_random_data += random_subsampling(temp_molecule_random_data, num_random_per_molecule)
+
+
+
+    list_subsample = setup["subsample_feature_list"]
+    temp_list_subsample = setup["subsample_feature_list"]
+    if temp_list_subsample == []:
+        for m in range(len(overall_subsampled_data[0])):
+            temp_list_subsample.append(m)
+
+    #if len(temp_list_subsample) <= 10:
+    #    overall_subsampled_data = subsampling_system(overall_subsampled_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]))
+    #else:
+    #    overall_subsampled_data = subsampling_system_with_PCA(overall_subsampled_data, list_desc = list_subsample, cutoff_sig = float(setup["subsample_cutoff_sig"]), rate = float(setup["subsample_rate"]),start_trial_component = 9)
+
+
+    #overall = overall_random_data + overall_subsampled_data
+    #overall = overall_subsampled_data
+    overall = overall_random_data
+
+
+    os.chdir(setup["working_dir"])
+    
+    temp = str(len(overall))
+    with open(temp + "_random_data.p", 'wb') as handle:
+        pickle.dump(random_data, handle, protocol=2)
+
+    return 
+
+
+if __name__ == "__main__":
+
+
+    setup_filename = sys.argv[1]
+    dataset_name = sys.argv[2]
+
+    with open(setup_filename) as f:
+        setup = json.load(f)
+
+
+    h = float(setup['grid_spacing'])
+    L = float(setup['box_dimension'])
+    N = int(setup['number_segment_per_side'])
+    dir_name = "{}_{}_{}".format(str(L).replace('.','-'),str(h).replace('.','-'),N)
+
+    working_dir = os.getcwd() + '/' + dir_name + '/' + dataset_name
+
+    setup["working_dir"] = working_dir
+
+    model_save_dir = working_dir + "/" + "NN_LDA_residual_1M_{}_{}_{}".format(setup["NN_setup"]["number_neuron_per_layer"], setup["NN_setup"]["number_layers"], setup["NN_setup"]["activation"])
+   
+    setup["model_save_dir"] = model_save_dir
+
+    
+    
+    get_training_data(dataset_name,setup)
+
+
+
+
+
+    
