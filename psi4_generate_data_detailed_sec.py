b332037a2735cadee3ac7bee4aca9050aedef9dc
ray38 <xlei38@gatech.edu>
2019-04-25 13:28:04 -0400

    test

--- psi4_generate_data_detailed_sec.py
+++ psi4_generate_data_detailed_sec.py
@@ -237,7 +237,7 @@ def process_system(molecule, molecule_name, xc, h, cell, num_blocks, sub_step, p
             for j in range(Ny):
                 for k in range(Nz):
                     process(count,X0 + x_step,Y0 + y_step,Z0 + z_step,x_inc,y_inc,z_inc,hx,hy,hz,i,j,k ,dv,scf_wfn,scf_e)
-                    count += 1
+        count += 1
 
     
     os.chdir(cwd) 
